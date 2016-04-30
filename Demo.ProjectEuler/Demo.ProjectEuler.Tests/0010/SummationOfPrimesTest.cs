403636c466d4d1cdb9701a551569f042996110d3
dance2die <dontbothermewithspam@gmail.com>
2016-04-30 15:48:59 -0400

    initial commit: SummationOfPrimesTest

---
+++ Demo.ProjectEuler/Demo.ProjectEuler.Tests/0010/SummationOfPrimesTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit.Abstractions;
+
+namespace Demo.ProjectEuler.Tests._0010
+{
+	public class SummationOfPrimesTest
+	{
+		private readonly ITestOutputHelper _output;
+
+		public SummationOfPrimesTest(ITestOutputHelper output)
+		{
+			_output = output;
+		}
+
+
+	}
+}
