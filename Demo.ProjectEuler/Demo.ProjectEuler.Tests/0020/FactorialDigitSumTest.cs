ee362d2af1f69bd1f10e64e9a9e55a3052c47274
dance2die <dontbothermewithspam@gmail.com>
2016-04-30 17:55:50 -0400

    Initial commit: FactorialDigitSumTest Implemented "TestFactorials"

---
+++ Demo.ProjectEuler/Demo.ProjectEuler.Tests/0020/FactorialDigitSumTest.cs
@@ -0,0 +1,51 @@
+﻿using Xunit;
+using Xunit.Abstractions;
+
+namespace Demo.ProjectEuler.Tests._0020
+{
+	public class FactorialDigitSumTest
+	{
+		private readonly ITestOutputHelper _output;
+		private readonly FactorialDigitSum _sut;
+
+		public FactorialDigitSumTest(ITestOutputHelper output)
+		{
+			_output = output;
+			_sut = new FactorialDigitSum();
+		}
+
+		[Theory]
+		[InlineData(-100, 1)]
+		[InlineData(0, 1)]
+		[InlineData(1, 1)]
+		[InlineData(2, 2)]
+		[InlineData(3, 6)]
+		[InlineData(4, 24)]
+		[InlineData(5, 120)]
+		[InlineData(6, 720)]
+		[InlineData(10, 3628800)]
+		public void TestFactorials(int input, float expectedResult)
+		{
+			float actualResult = _sut.GetFactorial(input);
+
+			Assert.Equal(expectedResult, actualResult);
+		}
+	}
+
+	public class FactorialDigitSum
+	{
+		public float GetFactorial(int input)
+		{
+			float result = 1;
+			if (input <= 1)
+				return 1;
+
+			for (int i = input; i > 0; i--)
+			{
+				result *= i;
+			}
+
+			return result;
+		}
+	}
+}
