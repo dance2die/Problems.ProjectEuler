4088f2c7b13debd718961ca39ff4dc9c89b14c1f
dance2die <dontbothermewithspam@gmail.com>
2016-04-30 11:50:44 -0400

    test pass with HARDCODED value

---
+++ Demo.ProjectEuler/Demo.ProjectEuler.Tests/0001/MultiplesOf3And5.cs
@@ -0,0 +1,10 @@
+namespace Demo.ProjectEuler.Tests._0001
+{
+	public class MultiplesOf3And5
+	{
+		public int CalculateUpTo(int upTo)
+		{
+			return 23;
+		}
+	}
+}
---
+++ Demo.ProjectEuler/Demo.ProjectEuler.Tests/0001/MultiplesOf3And5Test.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Demo.ProjectEuler.Tests._0001
+{
+	public class MultiplesOf3And5Test
+	{
+		private readonly ITestOutputHelper _output;
+
+		public MultiplesOf3And5Test(ITestOutputHelper output)
+		{
+			_output = output;
+		}
+
+		[Theory]
+		[InlineData(9, 23)]
+		public void TestSumUpTo(int upTo, int expectedResult)
+		{
+			var sut = new MultiplesOf3And5();
+			var actualResult = sut.CalculateUpTo(9);
+
+			Assert.Equal(expectedResult, actualResult);
+		}
+	}
+}
