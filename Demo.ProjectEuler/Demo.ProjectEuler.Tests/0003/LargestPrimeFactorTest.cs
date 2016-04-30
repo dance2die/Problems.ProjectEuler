9f97bcff6dd451f67b78fc16f0eb38a9d1baf004
dance2die <dontbothermewithspam@gmail.com>
2016-04-30 12:35:28 -0400

    Writing if XUnit passes list of array as an IEnumerable

--- Demo.ProjectEuler/Demo.ProjectEuler.Tests/0003/LargestPrimeFactorTest.cs
+++ Demo.ProjectEuler/Demo.ProjectEuler.Tests/0003/LargestPrimeFactorTest.cs
@@ -3,11 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
 
 namespace Demo.ProjectEuler.Tests._0003
 {
 	public class LargestPrimeFactorTest
 	{
+		private readonly ITestOutputHelper _output;
 
+		public LargestPrimeFactorTest(ITestOutputHelper output)
+		{
+			_output = output;
+		}
+
+		[Theory]
+		[InlineData(13195, new[] { 5, 7, 13, 29})]
+		public void TestGetPrimeFactors(int input, IEnumerable<int> expectedResult)
+		{
+			foreach (var item in expectedResult)
+			{
+				_output.WriteLine("Item: {0}", item);
+			}
+		}
 	}
 }
