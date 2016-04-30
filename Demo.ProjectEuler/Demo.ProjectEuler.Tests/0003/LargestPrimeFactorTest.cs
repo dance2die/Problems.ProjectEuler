a2c41b1031f59652d7b5da727b2b01ef0b98cf86
dance2die <dontbothermewithspam@gmail.com>
2016-04-30 12:28:04 -0400

    added a test for calculating largest prime factor - Question ID 3

---
+++ Demo.ProjectEuler/Demo.ProjectEuler.Tests/0003/LargestPrimeFactorTest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.ProjectEuler.Tests._0003
+{
+	public class LargestPrimeFactorTest
+	{
+
+	}
+}
