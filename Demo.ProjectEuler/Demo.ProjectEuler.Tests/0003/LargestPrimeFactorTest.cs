8e74eda652cac27a434ad9e4b4b150aa2d2c85b3
dance2die <dontbothermewithspam@gmail.com>
2016-04-30 12:48:58 -0400

    finished testing IsPrimeNumber

--- Demo.ProjectEuler/Demo.ProjectEuler.Tests/0003/LargestPrimeFactorTest.cs
+++ Demo.ProjectEuler/Demo.ProjectEuler.Tests/0003/LargestPrimeFactorTest.cs
@@ -18,13 +18,56 @@ namespace Demo.ProjectEuler.Tests._0003
 		}
 
 		[Theory]
-		[InlineData(13195, new[] { 5, 7, 13, 29})]
-		public void TestGetPrimeFactors(int input, IEnumerable<int> expectedResult)
+		[InlineData(-1000, false)]
+		[InlineData(-1, false)]
+		[InlineData(0, false)]
+		[InlineData(1, false)]
+		[InlineData(2, true)]
+		[InlineData(3, true)]
+		[InlineData(4, false)]
+		[InlineData(5, true)]
+		[InlineData(6, false)]
+		[InlineData(7, true)]
+		[InlineData(8, false)]
+		[InlineData(17, true)]
+		[InlineData(100, false)]
+		public void TestIsPrimeNumber(int value, bool expectedResult)
 		{
-			foreach (var item in expectedResult)
+			var sut = new LargestPrimeFactor();
+
+			bool actualResult = sut.IsPrimeNumber(value);
+
+			Assert.Equal(expectedResult, actualResult);
+		}
+
+		//[Theory]
+		//[InlineData(13195, new[] { 5, 7, 13, 29})]
+		//public void TestGetPrimeFactors(int value, IEnumerable<int> expectedResult)
+		//{
+		//	var sut = new LargestPrimeFactor();
+
+		//	HashSet<int> actualResult = sut.GetPrimeFactors(value);
+
+		//	Assert.True(actualResult.SetEquals(expectedResult));
+		//}
+	}
+
+	public class LargestPrimeFactor
+	{
+		//public HashSet<int> GetPrimeFactors(int value)
+		//{
+			
+		//}
+		public bool IsPrimeNumber(int value)
+		{
+			if (value <= 1) return false;	// by definion of Prime Number
+
+			for (int i = 2; i < value; i++)
 			{
-				_output.WriteLine("Item: {0}", item);
+				if (value % i == 0) return false;
 			}
+
+			return true;
 		}
 	}
 }
