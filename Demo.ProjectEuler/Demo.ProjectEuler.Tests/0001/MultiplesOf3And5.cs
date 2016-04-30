7f441a86dca7a90624c751e5841ff340164c391b
dance2die <dontbothermewithspam@gmail.com>
2016-04-30 12:17:50 -0400

    Created "TestMultiplesOf3And5Set" passed the tests

--- Demo.ProjectEuler/Demo.ProjectEuler.Tests/0001/MultiplesOf3And5.cs
+++ Demo.ProjectEuler/Demo.ProjectEuler.Tests/0001/MultiplesOf3And5.cs
@@ -1,10 +1,28 @@
+using System.Collections.Generic;
+
 namespace Demo.ProjectEuler.Tests._0001
 {
 	public class MultiplesOf3And5
 	{
-		public int CalculateUpTo(int upTo)
+		//public int CalculateBelow(int below)
+		//{
+		//	if (below <= 0) return 0;
+		//	if (below == 3) return 3;
+
+		//	return 23;
+		//}
+
+		public HashSet<int> GetMultiplesOf3And5Set(int below)
 		{
-			return 23;
+			HashSet<int> result = new HashSet<int>();
+
+			for (int i = 1; i < below; i++)
+			{
+				if (i % 3 == 0) result.Add(i);
+				if (i % 5 == 0) result.Add(i);
+			}
+
+			return result;
 		}
 	}
 }
--- Demo.ProjectEuler/Demo.ProjectEuler.Tests/0001/MultiplesOf3And5Test.cs
+++ Demo.ProjectEuler/Demo.ProjectEuler.Tests/0001/MultiplesOf3And5Test.cs
@@ -17,14 +17,37 @@ namespace Demo.ProjectEuler.Tests._0001
 			_output = output;
 		}
 
-		[Theory]
-		[InlineData(9, 23)]
-		public void TestSumUpTo(int upTo, int expectedResult)
+		[Fact]
+		public void TestMultiplesOf3And5Set()
 		{
-			var sut = new MultiplesOf3And5();
-			var actualResult = sut.CalculateUpTo(9);
+			const int below1 = 11;
+			const int below2 = 16;
 
-			Assert.Equal(expectedResult, actualResult);
+			HashSet<int> expectedSet1 = new HashSet<int> {3, 5, 6, 9, 10};
+			HashSet<int> expectedSet2 = new HashSet<int> {3, 5, 6, 9, 10, 12, 15};
+
+			var sut = new MultiplesOf3And5();
+
+			HashSet<int> actualSet1 = sut.GetMultiplesOf3And5Set(below1);
+			HashSet<int> actualSet2 = sut.GetMultiplesOf3And5Set(below2);
+
+			Assert.True(actualSet1.SetEquals(expectedSet1));
+			Assert.True(actualSet2.SetEquals(expectedSet2));
 		}
+
+		//[Theory]
+		//[InlineData(-1, 0)]
+		//[InlineData(0, 0)]
+		//[InlineData(3, 3)]
+		////[InlineData(5, 8)]
+		////[InlineData(6, 14)]
+		////[InlineData(9, 23)]
+		//public void TestSumUpTo(int upTo, int expectedResult)
+		//{
+		//	var sut = new MultiplesOf3And5();
+		//	var actualResult = sut.CalculateBelow(upTo);
+
+		//	Assert.Equal(expectedResult, actualResult);
+		//}
 	}
 }
