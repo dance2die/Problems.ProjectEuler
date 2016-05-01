439a776eaa549d4a0172e133cfbcb3f65522d53c
dance2die <dontbothermewithspam@gmail.com>
2016-04-30 22:28:46 -0400

    Initial commit for 0030

---
+++ Demo.ProjectEuler/Demo.ProjectEuler.Tests/0030/DigitFifthPowersTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Demo.ProjectEuler.Tests._0030
+{
+	public class DigitFifthPowersTest
+	{ 
+		private readonly ITestOutputHelper _output;
+		private readonly DigitFifthPowers _sut;
+
+		public DigitFifthPowersTest(ITestOutputHelper output)
+		{
+			_output = output;
+			_sut = new DigitFifthPowers();
+		}
+
+		[Theory]
+		[InlineData(new[] {1, 6, 3, 4}, 4, 1634)]
+		[InlineData(new[] {8, 2, 0, 8}, 4, 8208)]
+		[InlineData(new[] {9, 4, 7, 4}, 4, 9474)]
+		public void TestFourthPowerCalculation(IEnumerable<int> digits, int power, int expectedValue)
+		{
+			int actualValue = _sut.GetPoweredNumber(digits, power);
+
+			Assert.Equal(expectedValue, actualValue);
+		}
+	}
+
+	public class DigitFifthPowers
+	{
+		public int GetPoweredNumber(IEnumerable<int> digits, int power)
+		{
+			return digits.Sum(digit => (int) Math.Pow(digit, power));
+		}
+	}
+}
---
+++ Demo.ProjectEuler/Demo.ProjectEuler.Tests/0030/README.txt
@@ -0,0 +1,15 @@
+﻿https://projecteuler.net/problem=30
+
+
+Digit fifth powers
+Problem 30
+Surprisingly there are only three numbers that can be written as the sum of fourth powers of their digits:
+
+1634 = 14^4 + 64^4 + 34^4 + 44^4
+8208 = 84^4 + 24^4 + 04^4 + 84^4
+9474 = 94^4 + 44^4 + 74^4 + 44^4
+As 1 = 14 is not a sum it is not included.
+
+The sum of these numbers is 1634 + 8208 + 9474 = 19316.
+
+Find the sum of all the numbers that can be written as the sum of fifth powers of their digits.
