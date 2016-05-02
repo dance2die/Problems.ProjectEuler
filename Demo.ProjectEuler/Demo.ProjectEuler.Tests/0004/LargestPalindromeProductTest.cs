6a552fed0be9a081d9fb0df11ad9e11d64b334bd
dance2die <dontbothermewithspam@gmail.com>
2016-05-02 10:55:47 -0400

    initial commit of "LargestPalindromeProductTest"

---
+++ Demo.ProjectEuler/Demo.ProjectEuler.Tests/0004/LargestPalindromeProductTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit.Abstractions;
+
+namespace Demo.ProjectEuler.Tests._0004
+{
+	public class LargestPalindromeProductTest
+	{
+		private readonly ITestOutputHelper _output;
+
+		public LargestPalindromeProductTest(ITestOutputHelper output)
+		{
+			_output = output;
+		}
+
+
+	}
+}
