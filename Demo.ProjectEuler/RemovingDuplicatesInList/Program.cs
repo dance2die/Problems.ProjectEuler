248e21ad44ac093e98401d26e8d268f543a37914
dance2die <dontbothermewithspam@gmail.com>
2016-05-03 15:44:46 -0400

    Removed duplicate with less space

--- Demo.ProjectEuler/RemovingDuplicatesInList/Program.cs
+++ Demo.ProjectEuler/RemovingDuplicatesInList/Program.cs
@@ -7,6 +7,33 @@ namespace RemovingDuplicatesInList
 	{
 		public static void Main(string[] args)
 		{
+			//GetNoneDupeWithoutSpaceLimit();
+			GetNoneDupeWithSpaceLimit();
+		}
+
+		private static void GetNoneDupeWithSpaceLimit()
+		{
+			var dupeList = new List<int> { 2, 2, 1, 3, 4, 5, 3 };
+			dupeList.Sort();
+
+			// Starts with 1 because first element is always sorted.
+			for (int i = 1; i < dupeList.Count; i++)
+			{
+				if (dupeList[i] == dupeList[i - 1])
+				{
+					dupeList.RemoveAt(i);
+				}
+			}
+
+			foreach (int value in dupeList)
+			{
+				Console.Write("{0} ", value);
+			}
+			Console.WriteLine();
+		}
+
+		private static void GetNoneDupeWithoutSpaceLimit()
+		{
 			var dupeList = new List<int> {2, 2, 1, 3, 4, 5, 3};
 			var set = new List<int>();
 
