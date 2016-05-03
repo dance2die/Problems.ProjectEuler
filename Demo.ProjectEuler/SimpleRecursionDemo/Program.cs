c471c2bb3d2a47a3392844b97dacf6d844489169
dance2die <dontbothermewithspam@gmail.com>
2016-05-03 15:58:52 -0400

    simple recursion test.

---
+++ Demo.ProjectEuler/SimpleRecursionDemo/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace SimpleRecursionDemo
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("In order");
+			PrintRecursivelyInOrder(1, 3);
+			Console.WriteLine("Reverse");
+			PrintRecursivelyReverse(3);
+		}
+
+		private static void PrintRecursivelyInOrder(int value, int limit)
+		{
+			if (value <= limit)
+			{
+				PrintRecursivelyInOrder(value + 1, limit);
+				Console.WriteLine(value);
+			}
+		}
+
+		private static void PrintRecursivelyReverse(int value)
+		{
+			if (value >= 1)
+			{
+				PrintRecursivelyReverse(value - 1);
+				Console.WriteLine(value);
+			}
+		}
+	}
+}
