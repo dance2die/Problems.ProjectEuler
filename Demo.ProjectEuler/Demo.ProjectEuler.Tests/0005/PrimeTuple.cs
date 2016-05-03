496dd8e4e34d2cde7fd2502fae115838761cafee
dance2die <dontbothermewithspam@gmail.com>
2016-05-03 11:30:34 -0400

    Answer for ID 5 accepted on ProjectEuler.net

---
+++ Demo.ProjectEuler/Demo.ProjectEuler.Tests/0005/PrimeTuple.cs
@@ -0,0 +1,17 @@
+﻿namespace Demo.ProjectEuler.Tests._0005
+{
+	/// <summary>
+	/// Writable Integer tuple
+	/// </summary>
+	public class PrimeTuple
+	{
+		public int PrimeNumber { get; set; }
+		public int Power { get; set; }
+
+		public PrimeTuple(int primeNumber, int power)
+		{
+			PrimeNumber = primeNumber;
+			Power = power;
+		}
+	}
+}
