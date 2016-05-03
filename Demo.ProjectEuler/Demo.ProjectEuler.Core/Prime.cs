f86c9ecf4c7fb19093ae6d64d9af88c08117cf6c
dance2die <dontbothermewithspam@gmail.com>
2016-05-03 10:31:02 -0400

    Moved "GetPrimeFactors" to "Prime" so that I can reuse it for question #5

--- Demo.ProjectEuler/Demo.ProjectEuler.Core/Prime.cs
+++ Demo.ProjectEuler/Demo.ProjectEuler.Core/Prime.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 
 namespace Demo.ProjectEuler.Core
 {
@@ -17,5 +18,41 @@ namespace Demo.ProjectEuler.Core
 
 			return true;
 		}
+
+		public List<double> GetPrimeFactors(double value)
+		{
+			var result = new List<double>();
+
+			if (IsPrimeNumber(value))
+			{
+				result.Add(value);
+				return result;
+			}
+
+			double dividedValue = value;
+			for (int i = 2; i <= value / 2; i++)
+			{
+				if (IsPrimeNumber(i))
+				{
+					int remainder;
+					do
+					{
+						remainder = (int)(dividedValue % i);
+						double tempDividedValue = dividedValue / i;
+						if (remainder == 0)
+						{
+							result.Add(i);
+							dividedValue = tempDividedValue;
+						}
+					} while (remainder == 0 && dividedValue > 1);
+				}
+
+				if (dividedValue <= 1)
+					break;
+			}
+
+			return result;
+		}
+
 	}
 }
--- Demo.ProjectEuler/Demo.ProjectEuler.Tests/0003/LargestPrimeFactor.cs
+++ Demo.ProjectEuler/Demo.ProjectEuler.Tests/0003/LargestPrimeFactor.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections.Generic;
 using Demo.ProjectEuler.Core;
 
@@ -6,45 +5,16 @@ namespace Demo.ProjectEuler.Tests._0003
 {
 	public class LargestPrimeFactor
 	{
+		private readonly Prime _prime = new Prime();
+
 		public List<double> GetPrimeFactors(double value)
 		{
-			var result = new List<double>();
-
-			if (IsPrimeNumber(value))
-			{
-				result.Add(value);
-				return result;
-			}
-
-			double dividedValue = value;
-			for (int i = 2; i <= value / 2; i++)
-			{
-				if (IsPrimeNumber(i))
-				{
-					int remainder;
-					do
-					{
-						remainder = (int)(dividedValue % i);
-						double tempDividedValue = dividedValue / i;
-						if (remainder == 0)
-						{
-							result.Add(i);
-							dividedValue = tempDividedValue;
-						}
-					} while (remainder == 0 && dividedValue > 1);
-				}
-
-				if (dividedValue <= 1)
-					break;
-			}
-
-			return result;
+			return _prime.GetPrimeFactors(value);
 		}
 
 		public bool IsPrimeNumber(double value)
 		{
-			Prime prime = new Prime();
-			return prime.IsPrimeNumber(value);
+			return _prime.IsPrimeNumber(value);
 		}
 	}
 }
