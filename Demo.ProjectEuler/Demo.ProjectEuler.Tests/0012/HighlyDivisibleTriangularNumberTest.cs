d1aff384bb8e9c0e4c4a24121d68c372439aeaa6
dance2die <dontbothermewithspam@gmail.com>
2016-06-01 11:37:23 -0400

    PE-0012: Generates triangular number.

--- Demo.ProjectEuler/Demo.ProjectEuler.Tests/0012/HighlyDivisibleTriangularNumberTest.cs
+++ Demo.ProjectEuler/Demo.ProjectEuler.Tests/0012/HighlyDivisibleTriangularNumberTest.cs
@@ -1,13 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Demo.ProjectEuler.Tests.Core;
+﻿using Demo.ProjectEuler.Tests.Core;
+using Xunit;
 using Xunit.Abstractions;
 
 namespace Demo.ProjectEuler.Tests._0012
 {
+	/// <summary>
+	/// 1. Generate Triangular Numbers
+	/// 2. Find divisors for a number (regular factors, not prime factors)
+	/// 3. Test against 7 levels of triangular numbers
+	/// </summary>
 	public class HighlyDivisibleTriangularNumberTest : BaseTest
 	{
 		private readonly HighlyDivisibleTriangularNumber _sut = new HighlyDivisibleTriangularNumber();
@@ -15,10 +16,31 @@ namespace Demo.ProjectEuler.Tests._0012
 		public HighlyDivisibleTriangularNumberTest(ITestOutputHelper output) : base(output)
 		{
 		}
+
+		[Theory]
+		[InlineData(1, 1)]
+		[InlineData(2, 3)]
+		[InlineData(3, 6)]
+		[InlineData(4, 10)]
+		[InlineData(5, 15)]
+		[InlineData(6, 21)]
+		[InlineData(7, 28)]
+		public void TestGeneratingTriangularNumbers(int level, int expected)
+		{
+			int actual = _sut.GetTriangularNumbers(level);
+
+			Assert.Equal(expected, actual);
+		}
 	}
 
 	internal class HighlyDivisibleTriangularNumber
 	{
-
+		/// <remarks>
+		/// Formula on <see cref="https://en.wikipedia.org/wiki/Triangular_number"/>
+		/// </remarks>
+		public int GetTriangularNumbers(int level)
+		{
+			return (level * (level + 1)) / 2;
+		}
 	}
 }
