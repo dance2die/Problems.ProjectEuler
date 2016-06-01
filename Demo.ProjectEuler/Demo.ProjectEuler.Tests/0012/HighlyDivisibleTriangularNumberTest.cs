409b1e9a55e127b35fdf136269b6b48923ab63ff
dance2die <dontbothermewithspam@gmail.com>
2016-06-01 06:27:52 -0400

    PE-0012: Added a readme file

---
+++ Demo.ProjectEuler/Demo.ProjectEuler.Tests/0012/HighlyDivisibleTriangularNumberTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Demo.ProjectEuler.Tests.Core;
+using Xunit.Abstractions;
+
+namespace Demo.ProjectEuler.Tests._0012
+{
+	public class HighlyDivisibleTriangularNumberTest : BaseTest
+	{
+		private readonly HighlyDivisibleTriangularNumber _sut = new HighlyDivisibleTriangularNumber();
+
+		public HighlyDivisibleTriangularNumberTest(ITestOutputHelper output) : base(output)
+		{
+		}
+	}
+
+	internal class HighlyDivisibleTriangularNumber
+	{
+
+	}
+}
