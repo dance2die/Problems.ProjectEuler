849317dc6130a941aaca98feb8bc378d65b02a51
dance2die <dontbothermewithspam@gmail.com>
2016-06-02 14:28:01 -0400

    PE-0017: Created a test case with many inputs.

--- Demo.ProjectEuler/Demo.ProjectEuler.Tests/0017/NumberLetterCountsTest.cs
+++ Demo.ProjectEuler/Demo.ProjectEuler.Tests/0017/NumberLetterCountsTest.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using Demo.ProjectEuler.Tests.Core;
+using Xunit;
 using Xunit.Abstractions;
 
 namespace Demo.ProjectEuler.Tests._0017
@@ -11,6 +8,35 @@ namespace Demo.ProjectEuler.Tests._0017
 	public class NumberLetterCountsTest : BaseTest
 	{
 		private readonly NumberLetterCounts _sut = new NumberLetterCounts();
+
+		public NumberLetterCountsTest(ITestOutputHelper output) : base(output)
+		{
+		}
+
+		[Theory]
+		[InlineData(1, "One")]
+		[InlineData(7, "Seven")]
+		[InlineData(10, "Ten")]
+		[InlineData(11, "Elevent")]
+		[InlineData(12, "Twelve")]
+		[InlineData(21, "Twenty One")]
+		[InlineData(22, "Twenty Two")]
+		[InlineData(100, "One Hundred")]
+		[InlineData(101, "One Hundred And One")]
+		[InlineData(111, "One Hundred And Eleven")]
+		[InlineData(122, "One Hundered And Twenty Two")]
+		[InlineData(239, "Two Hundred And Thirty Nine")]
+		[InlineData(555, "Five Hundred And Fifty Five")]
+		[InlineData(999, "Nine Hundred And Ninety Nine")]
+		[InlineData(1000, "One Thousand")]
+		public void TestGeneratingNumberString(int number, int expected)
+		{
+			
+		}
+	}
+
+	internal class NumberLetterCounts
+	{
 		private readonly Dictionary<int, string> numberDictionary= new Dictionary<int, string>
 		{
 			[1] = "One", [2] = "Two", [3] = "Three", [4] = "Four", [5] = "Five",
@@ -20,13 +46,5 @@ namespace Demo.ProjectEuler.Tests._0017
 			[20] = "Twenty", [30] = "Thirty", [40] = "Forty", [50] = "Fifty", [60] = "Sixty",
 			[70] = "Seventy", [80] = "Eighty", [90] = "Ninety", [100] = "Hundred", [1000] = "Thousand",
 		};
-
-		public NumberLetterCountsTest(ITestOutputHelper output) : base(output)
-		{
-		}
-	}
-
-	internal class NumberLetterCounts
-	{
 	}
 }
