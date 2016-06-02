fc7b2cb58fce8b5a13f4d06a8b809320198a2d91
dance2die <dontbothermewithspam@gmail.com>
2016-06-02 12:26:59 -0400

    PE-0016: Answer accepted.

---
+++ Demo.ProjectEuler/Demo.ProjectEuler.Tests/0016/PowerDigitSumTest.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Numerics;
+using Demo.ProjectEuler.Tests.Core;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Demo.ProjectEuler.Tests._0016
+{
+	public class PowerDigitSumTest : BaseTest
+	{
+		private readonly PowerDigitSum _sut = new PowerDigitSum();
+
+		public PowerDigitSumTest(ITestOutputHelper output) : base(output)
+		{
+		}
+
+		[Fact]
+		public void TestSampleData()
+		{
+			const int power = 15;
+			BigInteger expected = 26;
+
+			BigInteger actual = _sut.GetPowerDigitSum(power);
+
+			Assert.Equal(expected, actual);
+		}
+
+		[Fact]
+		public void ShowResult()
+		{
+			const int power = 1000;
+			BigInteger result = _sut.GetPowerDigitSum(power);
+
+			_output.WriteLine(result.ToString(CultureInfo.InvariantCulture));
+		}
+	}
+
+	internal class PowerDigitSum
+	{
+		public BigInteger GetPowerDigitSum(int power)
+		{
+			BigInteger product = (BigInteger) Math.Pow(2, power);
+			string productString = product.ToString(CultureInfo.InvariantCulture);
+			var result = productString.Select(c => Convert.ToInt32(c.ToString())).Sum();
+
+			return result;
+		}
+	}
+}
