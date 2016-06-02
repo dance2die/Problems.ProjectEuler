6a1321ff5d8ce21d2f167d67a24e47dc467e15de
dance2die <dontbothermewithspam@gmail.com>
2016-06-02 11:23:25 -0400

    PE-0015: Refactor.

--- Demo.ProjectEuler/Demo.ProjectEuler.Tests/0015/LatticePathsTest.cs
+++ Demo.ProjectEuler/Demo.ProjectEuler.Tests/0015/LatticePathsTest.cs
@@ -33,42 +33,4 @@ namespace Demo.ProjectEuler.Tests._0015
 			_output.WriteLine(result.ToString());
 		}
 	}
-
-	internal class LatticePaths
-	{
-		/// <summary>
-		/// Combinations of walking lattice paths
-		/// </summary>
-		/// <remarks>
-		/// http://math.stackexchange.com/a/636133
-		/// </remarks>
-		public BigInteger GetLatticePaths(int size)
-		{
-			int topValue = size * 2;
-			int bottomValue = size;
-
-			// Get factorial until size is reached.
-			BigInteger result = 1;
-			for (int i = topValue; i > size; i--)
-			{
-				result *= i;
-			}
-
-			var bottomFactorial = GetFactorial(bottomValue);
-			result /= bottomFactorial;
-
-			return result;
-		}
-
-		private BigInteger GetFactorial(int value)
-		{
-			BigInteger result = 1;
-			for (int i = value; i > 1; i--)
-			{
-				result *= i;
-			}
-
-			return result;
-		}
-	}
 }
