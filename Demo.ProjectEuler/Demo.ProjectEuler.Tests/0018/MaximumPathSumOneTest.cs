3ae258ed0be5ec06b3bf8674a5a6a62b434fba1f
dance2die <dontbothermewithspam@gmail.com>
2016-06-04 13:33:15 -0400

    PE-0018: Refactor.

--- Demo.ProjectEuler/Demo.ProjectEuler.Tests/0018/MaximumPathSumOneTest.cs
+++ Demo.ProjectEuler/Demo.ProjectEuler.Tests/0018/MaximumPathSumOneTest.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
 using Demo.ProjectEuler.Tests.Core;
 using Xunit;
 using Xunit.Abstractions;
@@ -86,42 +84,6 @@ namespace Demo.ProjectEuler.Tests._0018
 			int result = _sut.GetMximumPathSum(input);
 
 			_output.WriteLine(result.ToString());
-		}
-	}
-
-	public class MaximumPathSum
-	{
-		public int GetMximumPathSum(string input)
-		{
-			List<IEnumerable<int>> matrix = ParseInput(input).ToList();
-
-			var currentLastRow = new List<int>();
-			for (int i = matrix.Count - 1; i >= 1; i--)
-			{
-				var currentLastRowCopy = currentLastRow.ToList();
-				var lastRow = currentLastRow.Count > 0 ? currentLastRowCopy : matrix[i].ToList();
-				var prevRow = matrix[i - 1].ToList();
-				currentLastRow.Clear();
-
-				for (int j = 0; j < prevRow.Count; j++)
-				{
-					var leftValue = lastRow[j] + prevRow[j];
-					var rightValue = lastRow[j + 1] + prevRow[j];
-
-					currentLastRow.Add(Math.Max(leftValue, rightValue));
-				}
-			}
-
-			return currentLastRow.Sum();
-		}
-
-		public IEnumerable<IEnumerable<int>> ParseInput(string input)
-		{
-			var lines = input.Split(new[] {Environment.NewLine}, StringSplitOptions.RemoveEmptyEntries);
-			foreach (string line in lines)
-			{
-				yield return line.Split(new[] {" "}, StringSplitOptions.RemoveEmptyEntries).Select(s => Convert.ToInt32(s));
-			}
 		}
 	}
 }
