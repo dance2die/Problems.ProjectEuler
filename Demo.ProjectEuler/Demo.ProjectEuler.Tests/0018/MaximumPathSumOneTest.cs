dbf13b21783c6f4e091179f6e77d66dbd71c7c60
dance2die <dontbothermewithspam@gmail.com>
2016-06-04 13:00:37 -0400

    PE-0018: Added Algorithm comment.

--- Demo.ProjectEuler/Demo.ProjectEuler.Tests/0018/MaximumPathSumOneTest.cs
+++ Demo.ProjectEuler/Demo.ProjectEuler.Tests/0018/MaximumPathSumOneTest.cs
@@ -1,14 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Demo.ProjectEuler.Tests.Core;
 using Xunit;
 using Xunit.Abstractions;
 
 namespace Demo.ProjectEuler.Tests._0018
 {
+	/// <summary>
+	/// Get the maximum sum from a triangular tree
+	/// </summary>
+	/// <remarks>
+	/// Algorithm
+	/// 
+	/// nlr (new last row) = List
+	/// for i = rows length = 1 to >= 1 i--
+	///		lr (last row) = if nlr length > 0 then nlr else rows[i]
+	///		pr (previous row) = row[i - 1]
+	///
+	///		for j = 0; j = pr length; j++
+	///			nlr[j] = Max( (lr[j] + pr[j], (lr[j+1] + pr[j]) )
+	/// 
+	/// return nlr.sum() // even though there is only one number
+	/// </remarks>
 	public class MaximumPathSumOneTest : BaseTest
 	{
 		private readonly MaximumPathSum _sut = new MaximumPathSum();
