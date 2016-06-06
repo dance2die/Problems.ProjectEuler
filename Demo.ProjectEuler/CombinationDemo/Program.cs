f72937fc83572998804fcf5ba94d1037455e23f8
dance2die <dontbothermewithspam@gmail.com>
2016-06-06 15:48:01 -0400

    uses bit manipulation instead of using stringified binary number.

--- Demo.ProjectEuler/CombinationDemo/Program.cs
+++ Demo.ProjectEuler/CombinationDemo/Program.cs
@@ -9,7 +9,7 @@ namespace CombinationDemo
 		static void Main(string[] args)
 		{
 			//GetCombination(new List<int> { 1, 2, 3 });
-			var list = new List<int> { 2, 5 };
+			var list = new List<int> { 1, 2, 3 };
 			IEnumerable<IEnumerable<int>> combinations = GetCombination2(list).ToList();
 			foreach (IEnumerable<int> combination in combinations)
 			{
@@ -27,7 +27,7 @@ namespace CombinationDemo
 			double count = Math.Pow(2, list.Count);
 			for (int i = 1; i <= count - 1; i++)
 			{
-				yield return GetNumbers(list, i).ToList();
+				yield return GetNumbers2(list, i);
 			}
 		}
 
@@ -38,6 +38,16 @@ namespace CombinationDemo
 			for (int j = 0; j < str.Length; j++)
 			{
 				if (str[j] == '1')
+					yield return list[j];
+			}
+		}
+
+		private static IEnumerable<T> GetNumbers2<T>(List<T> list, int index)
+		{
+			for (int j = 0; j < list.Count; j++)
+			{
+				// http://stackoverflow.com/a/19891145/4035
+				if ((index & (1 << j)) > 0)
 					yield return list[j];
 			}
 		}
