aade05636c967f62a1f8009cd813165abb750c0d
dance2die <dontbothermewithspam@gmail.com>
2016-06-06 06:20:04 -0400

    PE-0021: Created AmicableNumbersTest.

---
+++ Demo.ProjectEuler/Demo.ProjectEuler.Tests/0021/AmicableNumbersTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Demo.ProjectEuler.Tests.Core;
+using Xunit.Abstractions;
+
+namespace Demo.ProjectEuler.Tests._0021
+{
+	public class AmicableNumbersTest : BaseTest
+	{
+		private readonly AmicableNumbers _sut = new AmicableNumbers();
+
+		public AmicableNumbersTest(ITestOutputHelper output) : base(output)
+		{
+		}
+	}
+
+	public class AmicableNumbers
+	{
+	}
+}
