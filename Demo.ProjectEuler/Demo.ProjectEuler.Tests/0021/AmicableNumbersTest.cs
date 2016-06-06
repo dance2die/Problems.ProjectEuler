3189d48242b3ba348dc9c95be5644b74ac7c9744
dance2die <dontbothermewithspam@gmail.com>
2016-06-06 12:28:16 -0400

    PE-0021: TestBuildingDictionary passes.

--- Demo.ProjectEuler/Demo.ProjectEuler.Tests/0021/AmicableNumbersTest.cs
+++ Demo.ProjectEuler/Demo.ProjectEuler.Tests/0021/AmicableNumbersTest.cs
@@ -10,10 +10,16 @@ namespace Demo.ProjectEuler.Tests._0021
 	public class AmicableNumbersTest : BaseTest
 	{
 		private readonly AmicableNumbers _sut = new AmicableNumbers();
+
 		public static IEnumerable<object[]> DivisorNumberSumLookupData => new[]
 		{
-			new object[] { 220, new KeyValuePair<int, int>(220, 284)  },
-			new object[] { 284, new KeyValuePair<int, int>(284, 220)  }
+			new object[] {220, new KeyValuePair<int, int>(220, 284)},
+			new object[] {284, new KeyValuePair<int, int>(284, 220)}
+		};
+
+		public static IEnumerable<object[]> DictionaryData => new[]
+		{
+			new object[] {220, 284, new Dictionary<int, int> { [220] = 284, [284] = 220 } }
 		};
 
 		public AmicableNumbersTest(ITestOutputHelper output) : base(output)
@@ -39,11 +45,32 @@ namespace Demo.ProjectEuler.Tests._0021
 			Assert.True(expected.Key == actual.Key && expected.Value == actual.Value);
 			Assert.Equal(expected, actual);
 		}
+
+		[Theory]
+		[MemberData("DictionaryData")]
+		public void TestBuildingDictionary(int value1, int value2, IDictionary<int, int> expected)
+		{
+			Dictionary<int, int> actual = _sut.GetDivisorSumDictionary(new[] {value1, value2});
+
+			Assert.Equal(expected, actual);
+		}
 	}
 
 	public class AmicableNumbers
 	{
 		private readonly Factors _factors = new Factors();
+
+		public Dictionary<int, int> GetDivisorSumDictionary(IEnumerable<int> values)
+		{
+			Dictionary<int, int> result = new Dictionary<int, int>();
+			foreach (int value in values)
+			{
+				KeyValuePair<int, int> pairOfNumberSum = GetPairOfNumberSum(value);
+				result.Add(pairOfNumberSum.Key, pairOfNumberSum.Value);
+			}
+
+			return result;
+		}
 
 		public int GetDivisorSum(int value)
 		{
