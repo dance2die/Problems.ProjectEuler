70a4f70db0056f8d164d6f6d7b35289525962729
dance2die <dontbothermewithspam@gmail.com>
2016-06-07 09:23:56 -0400

    PE-0024: Created a test class LexicographicPermutationsTest

---
+++ Demo.ProjectEuler/Demo.ProjectEuler.Tests/0024/LexicographicPermutationsTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Demo.ProjectEuler.Tests.Core;
+using Xunit.Abstractions;
+
+namespace Demo.ProjectEuler.Tests._0024
+{
+	public class LexicographicPermutationsTest : BaseTest
+	{
+		private readonly LexicographicPermutations _sut = new LexicographicPermutations();
+
+		public LexicographicPermutationsTest(ITestOutputHelper output) : base(output)
+		{
+		}
+	}
+
+	public class LexicographicPermutations
+	{
+	}
+}
