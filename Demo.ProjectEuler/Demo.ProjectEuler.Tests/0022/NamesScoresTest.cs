66c8b561b26a35c3c9ae8af5cfa7af022b40b657
dance2die <dontbothermewithspam@gmail.com>
2016-06-06 20:36:59 -0400

    PE-0022: Answer Accepted.

--- Demo.ProjectEuler/Demo.ProjectEuler.Tests/0022/NamesScoresTest.cs
+++ Demo.ProjectEuler/Demo.ProjectEuler.Tests/0022/NamesScoresTest.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Numerics;
 using Demo.ProjectEuler.Tests.Core;
 using Xunit;
 using Xunit.Abstractions;
@@ -18,31 +19,11 @@ namespace Demo.ProjectEuler.Tests._0022
 		}
 
 		[Theory]
-		[InlineData('A', 1)]
-		[InlineData('B', 2)]
-		[InlineData('C', 3)]
-		[InlineData('D', 4)]
-		[InlineData('E', 5)]
-		[InlineData('F', 6)]
-		[InlineData('G', 7)]
-		[InlineData('H', 8)]
-		[InlineData('I', 9)]
-		[InlineData('J', 10)]
-		[InlineData('K', 11)]
-		[InlineData('L', 12)]
-		[InlineData('M', 13)]
-		[InlineData('N', 14)]
-		[InlineData('O', 15)]
-		[InlineData('P', 16)]
-		[InlineData('Q', 17)]
-		[InlineData('R', 18)]
-		[InlineData('S', 19)]
-		[InlineData('T', 20)]
-		[InlineData('U', 21)]
-		[InlineData('V', 22)]
-		[InlineData('W', 23)]
-		[InlineData('X', 24)]
-		[InlineData('Y', 25)]
+		[InlineData('A', 1)][InlineData('B', 2)][InlineData('C', 3)][InlineData('D', 4)][InlineData('E', 5)]
+		[InlineData('F', 6)][InlineData('G', 7)][InlineData('H', 8)][InlineData('I', 9)][InlineData('J', 10)]
+		[InlineData('K', 11)][InlineData('L', 12)][InlineData('M', 13)][InlineData('N', 14)][InlineData('O', 15)]
+		[InlineData('P', 16)][InlineData('Q', 17)][InlineData('R', 18)][InlineData('S', 19)][InlineData('T', 20)]
+		[InlineData('U', 21)][InlineData('V', 22)][InlineData('W', 23)][InlineData('X', 24)][InlineData('Y', 25)]
 		[InlineData('Z', 26)]
 		public void TestAlphabetNumberAssignment(char letter, int expected)
 		{
@@ -61,36 +42,40 @@ namespace Demo.ProjectEuler.Tests._0022
 			Assert.Equal(expected, actual);
 		}
 
-		//[Fact]
-		//public void TestParsedCOLINScore()
-		//{
-		//	var fileContent = File.ReadAllText(INPUT_FILE);
-		//	var names = fileContent
-		//		.Split(new[] {",", Environment.NewLine}, StringSplitOptions.RemoveEmptyEntries)
-		//		// remove double quotes around nammes.
-		//		.Select(name => name.Trim('"'))
-		//		.OrderBy(name => name)
-		//		.ToList();
+		[Fact]
+		public void ShowResult()
+		{
+			var fileContent = File.ReadAllText(INPUT_FILE);
+			var names = fileContent
+				.Split(new[] { ",", Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries)
+				// remove double quotes around nammes.
+				.Select(name => name.Trim('"'))
+				.OrderBy(name => name)
+				.ToList();
 
-		//	const int expected = 49714;
+			BigInteger actual = _sut.GetTotalNameScore(names);
 
-		//	int actual = _sut.GetNameScore(names);
-
-		//	Assert.Equal(expected, actual);
-		//}
+			_output.WriteLine(actual.ToString());
+		}
 	}
 
 	public class NamesScores
 	{
 		private const int OFFSET = 64;  // "A" is 65.
 
-		//public int GetNameScore(List<string> names)
-		//{
-		//	for (int i = 0; i < names.Count; i++)
-		//	{
+		public BigInteger GetTotalNameScore(List<string> names)
+		{
+			BigInteger result = 0;
+			for (int i = 0; i < names.Count; i++)
+			{
+				int position = i + 1;	// position is 1-based;
+				string name = names[i];
 
-		//	}
-		//}
+				result += position * GetNameWorth(name);
+			}
+
+			return result;
+		}
 
 		public int GetNameWorth(string name)
 		{
