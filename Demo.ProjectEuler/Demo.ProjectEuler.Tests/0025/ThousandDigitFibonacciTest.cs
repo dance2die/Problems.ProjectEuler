1b8968cde31ee3257952cdf8344dbd2c7cbccf9c
dance2die <dontbothermewithspam@gmail.com>
2016-06-07 21:01:35 -0400

    PE-0025: Added ThousandDigitFibonacciTest

---
+++ Demo.ProjectEuler/Demo.ProjectEuler.Tests/0025/ThousandDigitFibonacciTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Demo.ProjectEuler.Tests.Core;
+using Xunit.Abstractions;
+
+namespace Demo.ProjectEuler.Tests._0025
+{
+	public class ThousandDigitFibonacciTest : BaseTest
+	{
+		private readonly ThousandDigitFibonacci _sut = new ThousandDigitFibonacci();
+
+		public ThousandDigitFibonacciTest(ITestOutputHelper output) : base(output)
+		{
+		}
+	}
+
+	public class ThousandDigitFibonacci
+	{
+	}
+}
