afc9bb8f6a499cbb9a7f93af795c69ed812cad85
dance2die <dontbothermewithspam@gmail.com>
2016-06-11 11:06:55 -0400

    PE-0028: Still fails. Test condition is wrong.

--- Demo.ProjectEuler/Demo.ProjectEuler.Tests/0028/NumberSpiralDiagonalsTest.cs
+++ Demo.ProjectEuler/Demo.ProjectEuler.Tests/0028/NumberSpiralDiagonalsTest.cs
@@ -42,14 +42,47 @@ namespace Demo.ProjectEuler.Tests._0028
 		{
 			int[,] result = new int[dimension, dimension];
 			int startIndex = dimension/2;
+			int rowIndex = startIndex;
+			int colIndex = startIndex;
+			int currentValue = 1;
 
-			int prevRowLength = 0;
-			int prevColLength = 0;
+			int prevRowLength = 2;
+			int prevColLength = 2;
+
+			result[rowIndex, colIndex] = currentValue;
 
 			do
 			{
+				// +x until prevRowLength - 1
+				for (int i = 0; i < prevRowLength - 1; i++)
+				{
+					++rowIndex;
+					if (rowIndex >= dimension) return result;
+					result[rowIndex, colIndex] = ++currentValue;
+				}
+				prevRowLength++;
 
-			} while (prevRowLength < dimension && prevColLength < dimension);
+				// +y until prevColLength - 1
+				for (int j = 0; j < prevColLength - 1; j++)
+				{
+					result[rowIndex, ++colIndex] = ++currentValue;
+				}
+				prevColLength++;
+
+				// -x until prevRowLength - 1
+				for (int i2 = 0; i2 < prevRowLength - 1; i2++)
+				{
+					result[--rowIndex, colIndex] = ++currentValue;
+				}
+				prevRowLength++;
+
+				// -y until prevColLength - 1
+				for (int j2 = 0; j2 < prevColLength - 1; j2++)
+				{
+					result[rowIndex, --colIndex] = ++currentValue;
+				}
+				prevColLength++;
+			} while (prevRowLength <= dimension + 1 && prevColLength <= dimension + 1);
 
 			return result;
 		}
