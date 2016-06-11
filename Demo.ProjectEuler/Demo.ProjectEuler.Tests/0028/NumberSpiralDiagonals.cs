422f829c64dc6bb692256046c1df1de07f912348
dance2die <dontbothermewithspam@gmail.com>
2016-06-11 12:57:12 -0400

    PE-0028: Refactor.

---
+++ Demo.ProjectEuler/Demo.ProjectEuler.Tests/0028/NumberSpiralDiagonals.cs
@@ -0,0 +1,86 @@
+using System.Collections.Generic;
+
+namespace Demo.ProjectEuler.Tests._0028
+{
+	public class NumberSpiralDiagonals
+	{
+		public List<int> GetDiagonalNumbers(int dimension)
+		{
+			int[,] sprialNumbers = GetSpiralNumbers(dimension);
+			List<int> result = new List<int>();
+
+			const int firstRowIndex = 0;
+			int width = sprialNumbers.GetLength(firstRowIndex);
+
+			for (int i = 0; i < width; i++)
+			{
+				// Get left to right (\) diagonal numbers
+				var left = sprialNumbers[i, i];
+
+				// Get Right to left (/) diagonal numbers
+				var right = sprialNumbers[i, width - i - 1];
+
+				if (left == right) // add only one middle index number
+				{
+					result.Add(left);
+				}
+				else
+				{
+					result.Add(left);
+					result.Add(right);
+				}
+			}
+
+			return result;
+		}
+
+		public int[,] GetSpiralNumbers(int dimension)
+		{
+			int[,] result = new int[dimension, dimension];
+			int startIndex = dimension/2;
+			int rowIndex = startIndex;
+			int colIndex = startIndex;
+			int currentValue = 1;
+
+			int prevRowLength = 2;
+			int prevColLength = 2;
+
+			result[rowIndex, colIndex] = currentValue;
+
+			do
+			{
+				// +col until prevRowLength - 1
+				for (int i = 0; i < prevColLength - 1; i++)
+				{
+					++colIndex;
+					if (colIndex >= dimension) return result;
+					result[rowIndex, colIndex] = ++currentValue;
+				}
+				prevColLength++;
+
+				// +row until prevColLength - 1
+				for (int j = 0; j < prevRowLength - 1; j++)
+				{
+					result[++rowIndex, colIndex] = ++currentValue;
+				}
+				prevRowLength++;
+
+				// -col until prevRowLength - 1
+				for (int i2 = 0; i2 < prevColLength - 1; i2++)
+				{
+					result[rowIndex, --colIndex] = ++currentValue;
+				}
+				prevColLength++;
+
+				// -row until prevColLength - 1
+				for (int j2 = 0; j2 < prevRowLength - 1; j2++)
+				{
+					result[--rowIndex, colIndex] = ++currentValue;
+				}
+				prevRowLength++;
+			} while (prevRowLength <= dimension + 1 && prevColLength <= dimension + 1);
+
+			return result;
+		}
+	}
+}
