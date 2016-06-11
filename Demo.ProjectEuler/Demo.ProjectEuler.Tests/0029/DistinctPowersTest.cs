b121e9d85da7abdfc99ff0aeffeff6046e115acc
dance2die <dontbothermewithspam@gmail.com>
2016-06-11 14:37:58 -0400

    PE-0029: Added DistinctPowersTest

---
+++ Demo.ProjectEuler/Demo.ProjectEuler.Tests/0029/DistinctPowersTest.cs
@@ -0,0 +1,18 @@
+﻿using Demo.ProjectEuler.Tests.Core;
+using Xunit.Abstractions;
+
+namespace Demo.ProjectEuler.Tests._0029
+{
+	public class DistinctPowersTest : BaseTest
+	{
+		private readonly DistinctPowers _sut = new DistinctPowers();
+
+		public DistinctPowersTest(ITestOutputHelper output) : base(output)
+		{
+		}
+	}
+
+	public class DistinctPowers
+	{
+	}
+}
