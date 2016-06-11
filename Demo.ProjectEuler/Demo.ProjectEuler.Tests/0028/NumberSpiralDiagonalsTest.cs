dff80c383b62aa5e8ad7e42b73893c873ce66a10
dance2die <dontbothermewithspam@gmail.com>
2016-06-11 09:49:28 -0400

    committing

---
+++ Demo.ProjectEuler/Demo.ProjectEuler.Tests/0028/NumberSpiralDiagonalsTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Demo.ProjectEuler.Tests.Core;
+using Xunit.Abstractions;
+
+namespace Demo.ProjectEuler.Tests._0028
+{
+	public class NumberSpiralDiagonalsTest : BaseTest
+	{
+		private readonly NumberSpiralDiagonals _sut = new NumberSpiralDiagonals();
+
+		public NumberSpiralDiagonalsTest(ITestOutputHelper output) : base(output)
+		{
+		}
+	}
+
+	public class NumberSpiralDiagonals
+	{
+	}
+}
