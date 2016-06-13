5dff1c5a5271d5af6fd161bf17f6cbda922141b3
dance2die <dontbothermewithspam@gmail.com>
2016-06-13 06:12:05 -0400

    PE-0032: README and PandigitalProductsTest added.

---
+++ Demo.ProjectEuler/Demo.ProjectEuler.Tests/0032/PandigitalProductsTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Demo.ProjectEuler.Tests.Core;
+using Xunit.Abstractions;
+
+namespace Demo.ProjectEuler.Tests._0032
+{
+	public class PandigitalProductsTest : BaseTest
+	{
+		private readonly PandigitalProducts _sut = new PandigitalProducts();
+
+		public PandigitalProductsTest(ITestOutputHelper output) : base(output)
+		{
+		}
+	}
+
+	public class PandigitalProducts
+	{
+	}
+}
