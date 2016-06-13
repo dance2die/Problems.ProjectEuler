42f6f75ccf27c131bd1117e8d8684b89eeccaa62
dance2die <dontbothermewithspam@gmail.com>
2016-06-13 12:36:25 -0400

    PE-0032: Added a failing test, TestIsSpecialPandigital

--- Demo.ProjectEuler/Demo.ProjectEuler.Tests/0032/PandigitalProductsTest.cs
+++ Demo.ProjectEuler/Demo.ProjectEuler.Tests/0032/PandigitalProductsTest.cs
@@ -1,9 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Demo.ProjectEuler.Tests.Core;
+using Xunit;
 using Xunit.Abstractions;
 
 namespace Demo.ProjectEuler.Tests._0032
@@ -15,9 +12,22 @@ namespace Demo.ProjectEuler.Tests._0032
 		public PandigitalProductsTest(ITestOutputHelper output) : base(output)
 		{
 		}
+
+		[Fact]
+		public void TestIsSpecialPandigital()
+		{
+			var specialPandigitalCandidate = new Tuple<int, int, int>(39, 186, 7254);
+			bool isSpecialPandigital = _sut.IsSpecialPandigital(specialPandigitalCandidate);
+
+			Assert.True(isSpecialPandigital);
+		}
 	}
 
 	public class PandigitalProducts
 	{
+		public bool IsSpecialPandigital(Tuple<int, int, int> specialPandigitalCandidate)
+		{
+			return false;
+		}
 	}
 }
