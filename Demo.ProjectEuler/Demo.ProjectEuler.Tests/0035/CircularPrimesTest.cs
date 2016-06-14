284f2d538004a15d54aeae0bd61931912b2d756b
dance2die <dontbothermewithspam@gmail.com>
2016-06-14 09:19:18 -0400

    PE-0035: Getting primes under one million takes 2~3 seconds.

--- Demo.ProjectEuler/Demo.ProjectEuler.Tests/0035/CircularPrimesTest.cs
+++ Demo.ProjectEuler/Demo.ProjectEuler.Tests/0035/CircularPrimesTest.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Demo.ProjectEuler.Core;
 using Demo.ProjectEuler.Tests.Core;
+using Xunit;
 using Xunit.Abstractions;
 
 namespace Demo.ProjectEuler.Tests._0035
@@ -14,6 +16,24 @@ namespace Demo.ProjectEuler.Tests._0035
 
 		public CircularPrimesTest(ITestOutputHelper output) : base(output)
 		{
+		}
+
+		/// <remarks>
+		/// Runs in 2 seconds
+		/// </remarks>
+		[Fact]
+		public void TestGettingPrimesUnderOneMillion()
+		{
+			Prime prime = new Prime();
+			List<int> primes = new List<int>();
+
+			for (int i = 1; i <= 999999; i++)
+			{
+				if (prime.IsPrimeNumber(i))
+					primes.Add(i);
+			}
+
+			_output.WriteLine(primes.Count.ToString());
 		}
 	}
 
