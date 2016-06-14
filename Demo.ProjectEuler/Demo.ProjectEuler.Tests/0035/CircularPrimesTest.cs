37749b31bcc17521f8f8b0477b3d7d79bbd4dbe1
dance2die <dontbothermewithspam@gmail.com>
2016-06-14 06:42:11 -0400

    PE-0035: Added CircularPrimesTest

---
+++ Demo.ProjectEuler/Demo.ProjectEuler.Tests/0035/CircularPrimesTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Demo.ProjectEuler.Tests.Core;
+using Xunit.Abstractions;
+
+namespace Demo.ProjectEuler.Tests._0035
+{
+	public class CircularPrimesTest : BaseTest
+	{
+		private readonly CircularPrimes _sut = new CircularPrimes();
+
+		public CircularPrimesTest(ITestOutputHelper output) : base(output)
+		{
+		}
+	}
+
+	public class CircularPrimes
+	{
+	}
+}
