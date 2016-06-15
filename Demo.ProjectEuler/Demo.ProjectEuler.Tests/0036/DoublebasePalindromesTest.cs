272f2dabd8edd54fdb066b5f0230fea2c92d9bba
dance2die <dontbothermewithspam@gmail.com>
2016-06-15 11:40:31 -0400

    PE-0036: Added DoublebasePalindromesTest

---
+++ Demo.ProjectEuler/Demo.ProjectEuler.Tests/0036/DoublebasePalindromesTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Demo.ProjectEuler.Tests.Core;
+using Xunit.Abstractions;
+
+namespace Demo.ProjectEuler.Tests._0036
+{
+	public class DoublebasePalindromesTest : BaseTest
+	{
+		private readonly DoublebasePalindromes _sut = new DoublebasePalindromes();
+
+		public DoublebasePalindromesTest(ITestOutputHelper output) : base(output)
+		{
+		}
+	}
+
+	public class DoublebasePalindromes
+	{
+	}
+}
