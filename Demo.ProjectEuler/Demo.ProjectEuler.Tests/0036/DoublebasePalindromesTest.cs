9128d3f92622058c7a339cb8ffbc5e5e0c370c72
dance2die <dontbothermewithspam@gmail.com>
2016-06-16 10:57:49 -0400

    PE-0036: Test for generating binrary string passes.

--- Demo.ProjectEuler/Demo.ProjectEuler.Tests/0036/DoublebasePalindromesTest.cs
+++ Demo.ProjectEuler/Demo.ProjectEuler.Tests/0036/DoublebasePalindromesTest.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Demo.ProjectEuler.Tests.Core;
+using Xunit;
 using Xunit.Abstractions;
 
 namespace Demo.ProjectEuler.Tests._0036
@@ -15,9 +16,24 @@ namespace Demo.ProjectEuler.Tests._0036
 		public DoublebasePalindromesTest(ITestOutputHelper output) : base(output)
 		{
 		}
+
+		[Theory]
+		[InlineData(585, "1001001001")]
+		[InlineData(587, "1001001011")]
+		public void TestBinaryString(int value, string expected)
+		{
+			string actual = _sut.GetBinaryString(value);
+
+			Assert.Equal(expected, actual);
+		}
 	}
 
 	public class DoublebasePalindromes
 	{
+		public string GetBinaryString(int value)
+		{
+			const int radix = 2;
+			return Convert.ToString(value, radix);
+		}
 	}
 }
