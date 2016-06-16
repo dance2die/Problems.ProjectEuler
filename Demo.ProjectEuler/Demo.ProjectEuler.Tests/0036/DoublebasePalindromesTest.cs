d374e5a5cc78b53f90facbd27eea59f4964949df
dance2die <dontbothermewithspam@gmail.com>
2016-06-16 11:09:09 -0400

    PE-0036: Answer accepted.

--- Demo.ProjectEuler/Demo.ProjectEuler.Tests/0036/DoublebasePalindromesTest.cs
+++ Demo.ProjectEuler/Demo.ProjectEuler.Tests/0036/DoublebasePalindromesTest.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Numerics;
+using Demo.ProjectEuler.Core;
 using Demo.ProjectEuler.Tests.Core;
 using Xunit;
 using Xunit.Abstractions;
@@ -26,10 +24,35 @@ namespace Demo.ProjectEuler.Tests._0036
 
 			Assert.Equal(expected, actual);
 		}
+
+		[Fact]
+		public void ShowResult()
+		{
+			BigInteger result = _sut.GetPalindromeNumberSumUnderOneMillion();
+
+			_output.WriteLine(result.ToString());
+		}
 	}
 
 	public class DoublebasePalindromes
 	{
+		private readonly Palindrome _palindrome = new Palindrome();
+
+		public BigInteger GetPalindromeNumberSumUnderOneMillion()
+		{
+			BigInteger result = BigInteger.Zero;
+
+			for (int i = 1; i <= 1000000; i++)
+			{
+				string base10Text = i.ToString();
+				string base2Text = GetBinaryString(i);
+				if (_palindrome.IsPalindrome(base10Text) && _palindrome.IsPalindrome(base2Text))
+					result += i;
+			}
+
+			return result;
+		}
+
 		public string GetBinaryString(int value)
 		{
 			const int radix = 2;
