2fe38c49cb536675d198967ff1abc950321a270b
dance2die <dontbothermewithspam@gmail.com>
2016-06-16 22:20:06 -0400

    PE-0037: Added TruncatablePrimesTest

---
+++ Demo.ProjectEuler/Demo.ProjectEuler.Tests/0037/TruncatablePrimesTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Demo.ProjectEuler.Tests.Core;
+using Xunit.Abstractions;
+
+namespace Demo.ProjectEuler.Tests._0037
+{
+	public class TruncatablePrimesTest : BaseTest
+	{
+		private readonly TruncatablePrimes _sut = new TruncatablePrimes();
+
+		public TruncatablePrimesTest(ITestOutputHelper output) : base(output)
+		{
+		}
+	}
+
+	public class TruncatablePrimes
+	{
+	}
+}
