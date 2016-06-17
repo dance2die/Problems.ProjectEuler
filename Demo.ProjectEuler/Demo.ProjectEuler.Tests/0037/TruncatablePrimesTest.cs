e92af7da94355e0c4f18ca200e31beb3f756f425
dance2die <dontbothermewithspam@gmail.com>
2016-06-17 12:43:36 -0400

    PE-0037: takes too long. Need to use dynamic programming

--- Demo.ProjectEuler/Demo.ProjectEuler.Tests/0037/TruncatablePrimesTest.cs
+++ Demo.ProjectEuler/Demo.ProjectEuler.Tests/0037/TruncatablePrimesTest.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 using Demo.ProjectEuler.Core;
@@ -30,11 +31,45 @@ namespace Demo.ProjectEuler.Tests._0037
 
 			Assert.Equal(expected, actual);
 		}
+
+		[Fact]
+		public void ShowResult()
+		{
+			BigInteger result = _sut.GetTruncablePrimes();
+
+			_output.WriteLine(result.ToString());
+		}
 	}
 
 	public class TruncatablePrimes
 	{
 		private readonly Prime _prime = new Prime();
+
+		public BigInteger GetTruncablePrimes()
+		{
+			const int primeCountTotal = 11;	// given on the problem page.
+
+			BigInteger result = BigInteger.Zero;
+			int number = 1;
+			int counter = 0;
+
+			do
+			{
+				do
+				{
+					number++;
+					if (number == 3797)
+					{
+						Console.WriteLine(number);
+					}
+				} while (!IsTruncablePrime(number));
+
+				result += number;
+				counter++;
+			} while (counter <= 11);
+
+			return result;
+		}
 
 		public bool IsTruncablePrime(int prime)
 		{
@@ -45,10 +80,10 @@ namespace Demo.ProjectEuler.Tests._0037
 			string primeTextRightToLeft = primeTextLeftToRight;
 			// Remove number from left to right.
 			// Remove number from right to left.
-			for (int i = 0; i < primeTextLeftToRight.Length; i++)
+			for (int i = 0; i < prime.ToString().Length - 1; i++)
 			{
 				primeTextLeftToRight = primeTextLeftToRight.Substring(1);
-				primeTextRightToLeft = primeTextLeftToRight.Substring(0, primeTextRightToLeft.Length - 1);
+				primeTextRightToLeft = primeTextRightToLeft.Substring(0, primeTextRightToLeft.Length - 1);
 
 				double primeTextLeftToRightValue = Convert.ToDouble(primeTextLeftToRight);
 				double primeTextRightToLeftValue = Convert.ToDouble(primeTextRightToLeft);
