cfd58aee28b91650feaaa547b252ea6bf32cabcb
dance2die <dontbothermewithspam@gmail.com>
2016-06-17 12:07:55 -0400

    PE-0037: TestTruncablePrime tests IsTruncablePrime and passes.

--- Demo.ProjectEuler/Demo.ProjectEuler.Tests/0037/TruncatablePrimesTest.cs
+++ Demo.ProjectEuler/Demo.ProjectEuler.Tests/0037/TruncatablePrimesTest.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Demo.ProjectEuler.Core;
 using Demo.ProjectEuler.Tests.Core;
+using Xunit;
 using Xunit.Abstractions;
 
 namespace Demo.ProjectEuler.Tests._0037
@@ -15,9 +17,47 @@ namespace Demo.ProjectEuler.Tests._0037
 		public TruncatablePrimesTest(ITestOutputHelper output) : base(output)
 		{
 		}
+
+		[Theory]
+		[InlineData(2, false)]
+		[InlineData(3, false)]
+		[InlineData(5, false)]
+		[InlineData(7, false)]
+		[InlineData(3797, true)]
+		public void TestTruncablePrime(int prime, bool expected)
+		{
+			bool actual = _sut.IsTruncablePrime(prime);
+
+			Assert.Equal(expected, actual);
+		}
 	}
 
 	public class TruncatablePrimes
 	{
+		private readonly Prime _prime = new Prime();
+
+		public bool IsTruncablePrime(int prime)
+		{
+			if (prime < 10) return false;
+			if (!_prime.IsPrimeNumber(prime)) return false;
+
+			string primeTextLeftToRight = prime.ToString();
+			string primeTextRightToLeft = primeTextLeftToRight;
+			// Remove number from left to right.
+			// Remove number from right to left.
+			for (int i = 0; i < primeTextLeftToRight.Length; i++)
+			{
+				primeTextLeftToRight = primeTextLeftToRight.Substring(1);
+				primeTextRightToLeft = primeTextLeftToRight.Substring(0, primeTextRightToLeft.Length - 1);
+
+				double primeTextLeftToRightValue = Convert.ToDouble(primeTextLeftToRight);
+				double primeTextRightToLeftValue = Convert.ToDouble(primeTextRightToLeft);
+				if (!_prime.IsPrimeNumber(primeTextLeftToRightValue) || 
+					!_prime.IsPrimeNumber(primeTextRightToLeftValue))
+					return false;
+			}
+
+			return true;
+		}
 	}
 }
