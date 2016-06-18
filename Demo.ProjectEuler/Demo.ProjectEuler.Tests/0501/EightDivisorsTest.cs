800676f279a0ac914e2c1f06ce4df4565b3e9c81
dance2die <dontbothermewithspam@gmail.com>
2016-06-17 20:08:44 -0400

    PE-0501: Added "EightDivisorsTest"

---
+++ Demo.ProjectEuler/Demo.ProjectEuler.Tests/0501/EightDivisorsTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Demo.ProjectEuler.Tests.Core;
+using Xunit.Abstractions;
+
+namespace Demo.ProjectEuler.Tests._0501
+{
+	public class EightDivisorsTest : BaseTest
+	{
+		private readonly EightDivisors _sut = new EightDivisors();
+
+		public EightDivisorsTest(ITestOutputHelper output) : base(output)
+		{
+		}
+	}
+
+	public class EightDivisors
+	{
+	}
+}
