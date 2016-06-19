4b55a56aa82e2fe7bcd52192bafceb312d028b2f
dance2die <dontbothermewithspam@gmail.com>
2016-06-19 12:53:05 -0400

    PE-0501: TestHasEightDivisors passes.

--- Demo.ProjectEuler/Demo.ProjectEuler.Tests/0501/EightDivisorsTest.cs
+++ Demo.ProjectEuler/Demo.ProjectEuler.Tests/0501/EightDivisorsTest.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Demo.ProjectEuler.Core;
 using Demo.ProjectEuler.Tests.Core;
+using Xunit;
 using Xunit.Abstractions;
 
 namespace Demo.ProjectEuler.Tests._0501
@@ -15,9 +12,37 @@ namespace Demo.ProjectEuler.Tests._0501
 		public EightDivisorsTest(ITestOutputHelper output) : base(output)
 		{
 		}
+
+		[Theory]
+		[InlineData(24, true)]
+		[InlineData(30, true)]
+		[InlineData(40, true)]
+		[InlineData(42, true)]
+		[InlineData(54, true)]
+		[InlineData(56, true)]
+		[InlineData(66, true)]
+		[InlineData(70, true)]
+		[InlineData(78, true)]
+		[InlineData(88, true)]
+		[InlineData(31, false)]
+		[InlineData(44, false)]
+		[InlineData(55, false)]
+		public void TestHasEightDivisors(int value, bool expected)
+		{
+			bool actual = _sut.HasEdightDivisors(value);
+
+			Assert.Equal(expected, actual);
+		}
 	}
 
 	public class EightDivisors
 	{
+		private readonly Factors _factors = new Factors();
+
+		public bool HasEdightDivisors(int value)
+		{
+			const int factorCount = 8;
+			return _factors.GetFactorCount(value) == factorCount;
+		}
 	}
 }
