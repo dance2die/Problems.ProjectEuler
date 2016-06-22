ac8d2be05ec730b6e9c9b65ee1dbff68cd592d4a
dance2die <dontbothermewithspam@gmail.com>
2016-06-22 07:00:55 -0400

    PE-0040: Added README & ChampernownesConstantTest

---
+++ Demo.ProjectEuler/Demo.ProjectEuler.Tests/0040/ChampernownesConstantTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Demo.ProjectEuler.Tests.Core;
+using Xunit.Abstractions;
+
+namespace Demo.ProjectEuler.Tests._0040
+{
+	public class ChampernownesConstantTest : BaseTest
+	{
+		private readonly ChampernownesConstant _sut = new ChampernownesConstant();
+
+		public ChampernownesConstantTest(ITestOutputHelper output) : base(output)
+		{
+		}
+	}
+
+	public class ChampernownesConstant
+	{
+	}
+}
