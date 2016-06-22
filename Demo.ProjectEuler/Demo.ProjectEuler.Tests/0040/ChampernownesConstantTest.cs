091a1f0aae0e9ffb0dea8f10c1f7943eded75016
dance2die <dontbothermewithspam@gmail.com>
2016-06-22 15:12:04 -0400

    PE-0040: TestGettingDigitAt passes.

--- Demo.ProjectEuler/Demo.ProjectEuler.Tests/0040/ChampernownesConstantTest.cs
+++ Demo.ProjectEuler/Demo.ProjectEuler.Tests/0040/ChampernownesConstantTest.cs
@@ -25,10 +25,28 @@ namespace Demo.ProjectEuler.Tests._0040
 
 			Assert.Equal(expected, actual);
 		}
+
+		[Theory]
+		[InlineData(21, 9, "9")]
+		[InlineData(21, 10, "1")]
+		[InlineData(21, 11, "0")]
+		[InlineData(21, 12, "1")]
+		public void TestGettingDigitAt(int upto, int oneBasedIndex, string expected)
+		{
+			string stringNumber = _sut.GetNumberStringUpto(upto);
+			string actual = _sut.GetDigitAt(stringNumber, oneBasedIndex);
+
+			Assert.Equal(expected, actual);
+		}
 	}
 
 	public class ChampernownesConstant
 	{
+		public string GetDigitAt(string stringNumber, int oneBasedIndex)
+		{
+			return stringNumber[oneBasedIndex - 1].ToString();
+		}
+
 		public string GetNumberStringUpto(int upto)
 		{
 			return string.Join("", Enumerable.Range(1, upto).Select(number => number.ToString()));
