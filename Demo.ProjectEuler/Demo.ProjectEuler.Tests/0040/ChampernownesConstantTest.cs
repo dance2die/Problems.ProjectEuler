5ccfa5c04a3393c5ee0907c86438c9f725a02a3e
dance2die <dontbothermewithspam@gmail.com>
2016-06-22 15:15:40 -0400

    PE-0040: changed return type from string to int for "GetDigitValueAt"

--- Demo.ProjectEuler/Demo.ProjectEuler.Tests/0040/ChampernownesConstantTest.cs
+++ Demo.ProjectEuler/Demo.ProjectEuler.Tests/0040/ChampernownesConstantTest.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using Demo.ProjectEuler.Tests.Core;
 using Xunit;
 using Xunit.Abstractions;
@@ -27,24 +28,41 @@ namespace Demo.ProjectEuler.Tests._0040
 		}
 
 		[Theory]
-		[InlineData(21, 9, "9")]
-		[InlineData(21, 10, "1")]
-		[InlineData(21, 11, "0")]
-		[InlineData(21, 12, "1")]
-		public void TestGettingDigitAt(int upto, int oneBasedIndex, string expected)
+		[InlineData(21, 9, 9)]
+		[InlineData(21, 10, 1)]
+		[InlineData(21, 11, 0)]
+		[InlineData(21, 12, 1)]
+		public void TestGettingDigitAt(int upto, int oneBasedIndex, int expected)
 		{
 			string stringNumber = _sut.GetNumberStringUpto(upto);
-			string actual = _sut.GetDigitAt(stringNumber, oneBasedIndex);
+			int actual = _sut.GetDigitValueAt(stringNumber, oneBasedIndex);
 
 			Assert.Equal(expected, actual);
 		}
+
+		//[Fact]
+		//public void ShowResult()
+		//{
+		//	const int upto = 1000000;
+		//	string stringNumber = _sut.GetNumberStringUpto(upto);
+
+		//	int value1 = _sut.GetDigitAt(stringNumber, 1);
+		//	int value2 = _sut.GetDigitAt(stringNumber, 10);
+		//	int value3 = _sut.GetDigitAt(stringNumber, 100);
+		//	int value4 = _sut.GetDigitAt(stringNumber, 1000);
+		//	int value5 = _sut.GetDigitAt(stringNumber, 10000);
+		//	int value6 = _sut.GetDigitAt(stringNumber, 100000);
+		//	int value7 = _sut.GetDigitAt(stringNumber, 1000000);
+
+
+		//}
 	}
 
 	public class ChampernownesConstant
 	{
-		public string GetDigitAt(string stringNumber, int oneBasedIndex)
+		public int GetDigitValueAt(string stringNumber, int oneBasedIndex)
 		{
-			return stringNumber[oneBasedIndex - 1].ToString();
+			return Convert.ToInt32(stringNumber[oneBasedIndex - 1].ToString());
 		}
 
 		public string GetNumberStringUpto(int upto)
