2dbfb0cecc7a6e60d9a16b8defe89dfee595f6bb
dance2die <dontbothermewithspam@gmail.com>
2016-06-23 06:30:12 -0400

    PE-0042: Added "CodedTriangleNumbers" and README file.

---
+++ Demo.ProjectEuler/Demo.ProjectEuler.Tests/0042/CodedTriangleNumbersTest.cs
@@ -0,0 +1,18 @@
+﻿using Demo.ProjectEuler.Tests.Core;
+using Xunit.Abstractions;
+
+namespace Demo.ProjectEuler.Tests._0042
+{
+	public class CodedTriangleNumbersTest : BaseTest
+	{
+		private readonly CodedTriangleNumbers _sut = new CodedTriangleNumbers();
+
+		public CodedTriangleNumbersTest(ITestOutputHelper output) : base(output)
+		{
+		}
+	}
+
+	public class CodedTriangleNumbers
+	{
+	}
+}
