70f6a92c34a48280e1f91c27e0f5da5d68f60883
dance2die <dontbothermewithspam@gmail.com>
2016-06-25 10:40:39 -0400

    PE-0042: TestGettingCharacterPosition passes.

--- Demo.ProjectEuler/Demo.ProjectEuler.Tests/0042/CodedTriangleNumbersTest.cs
+++ Demo.ProjectEuler/Demo.ProjectEuler.Tests/0042/CodedTriangleNumbersTest.cs
@@ -1,8 +1,16 @@
 ﻿using Demo.ProjectEuler.Tests.Core;
+using Xunit;
 using Xunit.Abstractions;
 
 namespace Demo.ProjectEuler.Tests._0042
 {
+	/// <summary>
+	/// Test Scenarios
+	/// 
+	/// Get Position of a character in English.
+	/// 
+	/// Calculate a triangle number for a given number
+	/// </summary>
 	public class CodedTriangleNumbersTest : BaseTest
 	{
 		private readonly CodedTriangleNumbers _sut = new CodedTriangleNumbers();
@@ -10,9 +18,31 @@ namespace Demo.ProjectEuler.Tests._0042
 		public CodedTriangleNumbersTest(ITestOutputHelper output) : base(output)
 		{
 		}
+
+		[Theory]
+		[InlineData('A', 1)]
+		[InlineData('B', 2)]
+		[InlineData('C', 3)]
+		[InlineData('D', 4)]
+		[InlineData('E', 5)]
+		[InlineData('N', 14)]
+		[InlineData('X', 24)]
+		[InlineData('Y', 25)]
+		[InlineData('Z', 26)]
+		public void TestGettingCharacterPosition(char character, int expected)
+		{
+			int actual = _sut.GetPosition(character);
+
+			Assert.Equal(expected, actual);
+		}
 	}
 
 	public class CodedTriangleNumbers
 	{
+		public int GetPosition(char character)
+		{
+			const int offset = 64;	// 'A' is 65 in ASCII table.
+			return character - offset;
+		}
 	}
 }
