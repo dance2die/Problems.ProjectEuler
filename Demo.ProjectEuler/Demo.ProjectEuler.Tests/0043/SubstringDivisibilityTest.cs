c64afd4bc172a26f5192a37ea9bf5d5f13e8a132
dance2die <dontbothermewithspam@gmail.com>
2016-06-25 11:39:26 -0400

    PE-0043: TestPermutationOfPandigitalNumbers passes.

--- Demo.ProjectEuler/Demo.ProjectEuler.Tests/0043/SubstringDivisibilityTest.cs
+++ Demo.ProjectEuler/Demo.ProjectEuler.Tests/0043/SubstringDivisibilityTest.cs
@@ -3,17 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Demo.ProjectEuler.Core;
 using Demo.ProjectEuler.Tests.Core;
+using Xunit;
 using Xunit.Abstractions;
 
 namespace Demo.ProjectEuler.Tests._0043
 {
+	/// <summary>
+	/// Test scenarios
+	/// 
+	/// Generate permutation of pandigital numbers using 0~9.
+	/// 
+	/// Test that 1406357289 has the special properties.
+	/// </summary>
 	public class SubstringDivisibilityTest : BaseTest
 	{
 		private readonly SubstringDivisibility _sut = new SubstringDivisibility();
 
 		public SubstringDivisibilityTest(ITestOutputHelper output) : base(output)
 		{
+		}
+
+		[Fact]
+		public void TestPermutationOfPandigitalNumbers()
+		{
+			Permutation permutation = new Permutation();
+			var permutations = permutation.GetPermutations(Enumerable.Range(0, 9).ToList()).ToList();
+			_output.WriteLine("Count: {0}", permutations.Count);
+
+			const int expected = 362880;
+			Assert.Equal(expected, permutations.Count);
 		}
 	}
 
