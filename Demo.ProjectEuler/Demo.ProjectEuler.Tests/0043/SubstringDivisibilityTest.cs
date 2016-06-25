bf23ab758ab6f53e5bb5e234713ab56577c892d0
dance2die <dontbothermewithspam@gmail.com>
2016-06-25 11:33:05 -0400

    PE-0043: Added SubstringDivisibilityTest

---
+++ Demo.ProjectEuler/Demo.ProjectEuler.Tests/0043/SubstringDivisibilityTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Demo.ProjectEuler.Tests.Core;
+using Xunit.Abstractions;
+
+namespace Demo.ProjectEuler.Tests._0043
+{
+	public class SubstringDivisibilityTest : BaseTest
+	{
+		private readonly SubstringDivisibility _sut = new SubstringDivisibility();
+
+		public SubstringDivisibilityTest(ITestOutputHelper output) : base(output)
+		{
+		}
+	}
+
+	public class SubstringDivisibility
+	{
+	}
+}
