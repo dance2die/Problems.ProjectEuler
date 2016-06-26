fc5cf867f8e00daa8008fd7305d5d5e2685ae8ff
dance2die <dontbothermewithspam@gmail.com>
2016-06-25 20:04:06 -0400

    PE-0043: Answer Accepted. Bruite forced.

--- Demo.ProjectEuler/Demo.ProjectEuler.Tests/0043/SubstringDivisibilityTest.cs
+++ Demo.ProjectEuler/Demo.ProjectEuler.Tests/0043/SubstringDivisibilityTest.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using Demo.ProjectEuler.Core;
 using Demo.ProjectEuler.Tests.Core;
 using Xunit;
@@ -26,10 +28,10 @@ namespace Demo.ProjectEuler.Tests._0043
 		public void TestPermutationOfPandigitalNumbers()
 		{
 			Permutation permutation = new Permutation();
-			var permutations = permutation.GetPermutations(Enumerable.Range(0, 9).ToList()).ToList();
+			var permutations = permutation.GetPermutations(Enumerable.Range(0, 10).ToList()).ToList();
 			_output.WriteLine("Count: {0}", permutations.Count);
 
-			const int expected = 362880;
+			const int expected = 3628800;
 			Assert.Equal(expected, permutations.Count);
 		}
 
@@ -43,18 +45,50 @@ namespace Demo.ProjectEuler.Tests._0043
 
 			Assert.Equal(expected, actual);
 		}
+
+		[Fact]
+		public void ShowResult()
+		{
+			BigInteger result = _sut.GetNumbersWithSpecialProperties();
+
+			_output.WriteLine(result.ToString());
+		}
 	}
 
 	public class SubstringDivisibility
 	{
 		private readonly int[] _primes = {2, 3, 5, 7, 11, 13, 17};
 
-		public bool HasSpecialProperties(int input)
+		public BigInteger GetNumbersWithSpecialProperties()
+		{
+			Permutation perm = new Permutation();
+			var permutations = perm.GetPermutations(Enumerable.Range(0, 10).ToList()).ToList();
+
+			BigInteger result = BigInteger.Zero;
+			foreach (IEnumerable<int> permutation in permutations)
+			{
+				string numberText = string.Join("", permutation.Select(n => n.ToString()).ToArray());
+				if (numberText.StartsWith("0")) continue;
+
+				BigInteger number = BigInteger.Parse(numberText);
+
+				if (HasSpecialProperties(number))
+					result += number;
+			}
+
+			return result;
+		}
+
+		public bool HasSpecialProperties(BigInteger input)
 		{
 			string inputText = input.ToString();
 			for (int i = 0; i < inputText.Length - 3; i++)
 			{
-				string substring = $"{inputText[i + 1]}{inputText[i + 2]}{inputText[i + 3]}";
+				var digit1 = inputText[i + 1];
+				var digit2 = inputText[i + 2];
+				var digit3 = inputText[i + 3];
+
+				string substring = $"{digit1}{digit2}{digit3}";
 				int number = Convert.ToInt32(substring);
 
 				if (number % _primes[i] != 0)
