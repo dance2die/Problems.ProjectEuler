c75541f8c54b648f939dc3a936aa6cb66b00d4e5
dance2die <dontbothermewithspam@gmail.com>
2016-06-25 20:05:36 -0400

    PE-0043: Refactor.

--- Demo.ProjectEuler/Demo.ProjectEuler.Tests/0043/SubstringDivisibilityTest.cs
+++ Demo.ProjectEuler/Demo.ProjectEuler.Tests/0043/SubstringDivisibilityTest.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
 using System.Numerics;
 using Demo.ProjectEuler.Core;
 using Demo.ProjectEuler.Tests.Core;
@@ -52,50 +50,6 @@ namespace Demo.ProjectEuler.Tests._0043
 			BigInteger result = _sut.GetNumbersWithSpecialProperties();
 
 			_output.WriteLine(result.ToString());
-		}
-	}
-
-	public class SubstringDivisibility
-	{
-		private readonly int[] _primes = {2, 3, 5, 7, 11, 13, 17};
-
-		public BigInteger GetNumbersWithSpecialProperties()
-		{
-			Permutation perm = new Permutation();
-			var permutations = perm.GetPermutations(Enumerable.Range(0, 10).ToList()).ToList();
-
-			BigInteger result = BigInteger.Zero;
-			foreach (IEnumerable<int> permutation in permutations)
-			{
-				string numberText = string.Join("", permutation.Select(n => n.ToString()).ToArray());
-				if (numberText.StartsWith("0")) continue;
-
-				BigInteger number = BigInteger.Parse(numberText);
-
-				if (HasSpecialProperties(number))
-					result += number;
-			}
-
-			return result;
-		}
-
-		public bool HasSpecialProperties(BigInteger input)
-		{
-			string inputText = input.ToString();
-			for (int i = 0; i < inputText.Length - 3; i++)
-			{
-				var digit1 = inputText[i + 1];
-				var digit2 = inputText[i + 2];
-				var digit3 = inputText[i + 3];
-
-				string substring = $"{digit1}{digit2}{digit3}";
-				int number = Convert.ToInt32(substring);
-
-				if (number % _primes[i] != 0)
-					return false;
-			}
-
-			return true;
 		}
 	}
 }
