90342b37c5fb3d18e6ee7aa9260c1acc0c7e0953
dance2die <dontbothermewithspam@gmail.com>
2016-06-26 09:35:04 -0400

    PE-0045: Added NextTriangleTest

---
+++ Demo.ProjectEuler/Demo.ProjectEuler.Tests/0045/NextTriangleTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Demo.ProjectEuler.Tests.Core;
+using Xunit.Abstractions;
+
+namespace Demo.ProjectEuler.Tests._0045
+{
+	public class NextTriangleTest : BaseTest
+	{
+		private readonly NextTriangle _sut = new NextTriangle();
+
+		public NextTriangleTest(ITestOutputHelper output) : base(output)
+		{
+		}
+	}
+
+	public class NextTriangle
+	{
+	}
+}
