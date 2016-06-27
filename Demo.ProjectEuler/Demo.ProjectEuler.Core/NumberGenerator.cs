5edce9d08e0123cc94dd00254d1da0da21394ef5
dance2die <dontbothermewithspam@gmail.com>
2016-06-27 13:02:56 -0400

    PE-0045: Created TestPentagonalNumberGeneration

--- Demo.ProjectEuler/Demo.ProjectEuler.Core/NumberGenerator.cs
+++ Demo.ProjectEuler/Demo.ProjectEuler.Core/NumberGenerator.cs
@@ -2,9 +2,16 @@ namespace Demo.ProjectEuler.Core
 {
 	public class NumberGenerator
 	{
+		/// <param name="n">One-based index</param>
 		public int GetTriangleNumber(int n)
 		{
 			return n * (n + 1) / 2;
+		}
+
+		/// <param name="n">One-based index</param>
+		public int GetPentagonalNumber(int n)
+		{
+			return n * (3 * n - 1) / 2;
 		}
 	}
 }
--- Demo.ProjectEuler/Demo.ProjectEuler.Tests/0045/NextTriangleTest.cs
+++ Demo.ProjectEuler/Demo.ProjectEuler.Tests/0045/NextTriangleTest.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Demo.ProjectEuler.Core;
 using Demo.ProjectEuler.Tests.Core;
+using Xunit;
 using Xunit.Abstractions;
 
 namespace Demo.ProjectEuler.Tests._0045
@@ -14,6 +11,19 @@ namespace Demo.ProjectEuler.Tests._0045
 
 		public NextTriangleTest(ITestOutputHelper output) : base(output)
 		{
+		}
+
+		[Theory]
+		[InlineData(1, 1)]
+		[InlineData(2, 5)]
+		[InlineData(3, 12)]
+		[InlineData(4, 22)]
+		[InlineData(5, 35)]
+		public void TestPentagonalNumberGeneration(int oneBasedIndex, int expected)
+		{
+			int actual = new NumberGenerator().GetPentagonalNumber(oneBasedIndex);
+
+			Assert.Equal(expected, actual);
 		}
 	}
 
