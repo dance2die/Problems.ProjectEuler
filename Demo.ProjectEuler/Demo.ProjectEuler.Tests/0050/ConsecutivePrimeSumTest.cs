00d4b81f1b5ac2ee9ff527ded98ce66ee3b083b4
dance2die <dontbothermewithspam@gmail.com>
2016-06-28 13:00:25 -0400

    PE-0050: Added ConsecutivePrimeSumTest

---
+++ Demo.ProjectEuler/Demo.ProjectEuler.Tests/0050/ConsecutivePrimeSumTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Demo.ProjectEuler.Tests.Core;
+using Xunit.Abstractions;
+
+namespace Demo.ProjectEuler.Tests._0050
+{
+	public class ConsecutivePrimeSumTest : BaseTest
+	{
+		private readonly ConsecutivePrimeSum _sut = new ConsecutivePrimeSum();
+
+		public ConsecutivePrimeSumTest(ITestOutputHelper output) : base(output)
+		{
+		}
+	}
+
+	public class ConsecutivePrimeSum
+	{
+	}
+}
