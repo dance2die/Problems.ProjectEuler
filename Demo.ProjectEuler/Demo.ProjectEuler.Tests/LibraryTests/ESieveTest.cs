063dc948dc50b225e916ff64eb59a8bb7ff88c14
dance2die <dontbothermewithspam@gmail.com>
2016-06-29 09:45:11 -0400

    PE-LibraryTest: added ESieveTest

---
+++ Demo.ProjectEuler/Demo.ProjectEuler.Tests/LibraryTests/ESieveTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Demo.ProjectEuler.Tests.Core;
+using Xunit.Abstractions;
+
+namespace Demo.ProjectEuler.Tests.LibraryTests
+{
+	public class ESieveTest : BaseTest
+	{
+		private readonly ESieve _sut = new ESieve();
+
+		public ESieveTest(ITestOutputHelper output) : base(output)
+		{
+		}
+	}
+
+	public class ESieve
+	{
+	}
+}
