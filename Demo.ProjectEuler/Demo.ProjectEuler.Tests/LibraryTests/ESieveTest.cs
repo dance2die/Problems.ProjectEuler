9256af00937ed150f52cbed62560da781d4b9f44
dance2die <dontbothermewithspam@gmail.com>
2016-06-29 16:22:24 -0400

    PE-LibraryTest: TestGeneratingPrimes fails.

--- Demo.ProjectEuler/Demo.ProjectEuler.Tests/LibraryTests/ESieveTest.cs
+++ Demo.ProjectEuler/Demo.ProjectEuler.Tests/LibraryTests/ESieveTest.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Demo.ProjectEuler.Tests.Core;
+using Xunit;
 using Xunit.Abstractions;
 
 namespace Demo.ProjectEuler.Tests.LibraryTests
@@ -15,9 +16,32 @@ namespace Demo.ProjectEuler.Tests.LibraryTests
 		public ESieveTest(ITestOutputHelper output) : base(output)
 		{
 		}
+
+		/// <remarks>
+		/// Generated expected primes using 
+		/// http://www.free-online-calculator-use.com/prime-number-generator.html
+		/// </remarks>
+		[Theory]
+		[InlineData(2, new [] { 2 })]
+		[InlineData(10, new [] { 2, 3, 5, 7 })]
+		[InlineData(50, new [] { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47 })]
+		[InlineData(100, new [] { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97 })]
+		[InlineData(200, new [] { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199 })]
+		public void TestGeneratingPrimes(int limit, int[] expected)
+		{
+			int[] actual = _sut.GetPrimes(limit);
+
+			Assert.True(expected.SequenceEqual(actual));
+		}
 	}
 
 	public class ESieve
 	{
+		public int[] GetPrimes(int limit)
+		{
+			var primes = new int[]{};
+
+			return primes;
+		}
 	}
 }
