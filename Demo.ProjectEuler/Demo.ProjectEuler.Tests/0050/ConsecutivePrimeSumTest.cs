d22c516bb93b2c9ae7cb170e280b3113be486943
dance2die <dontbothermewithspam@gmail.com>
2016-07-01 17:46:14 -0400

    PE-0050: Created a failing test, TestSampleData

--- Demo.ProjectEuler/Demo.ProjectEuler.Tests/0050/ConsecutivePrimeSumTest.cs
+++ Demo.ProjectEuler/Demo.ProjectEuler.Tests/0050/ConsecutivePrimeSumTest.cs
@@ -4,10 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Demo.ProjectEuler.Tests.Core;
+using Xunit;
 using Xunit.Abstractions;
 
 namespace Demo.ProjectEuler.Tests._0050
 {
+	/// <summary>
+	/// Test Scenarios.
+	/// </summary>
 	public class ConsecutivePrimeSumTest : BaseTest
 	{
 		private readonly ConsecutivePrimeSum _sut = new ConsecutivePrimeSum();
@@ -15,9 +19,24 @@ namespace Demo.ProjectEuler.Tests._0050
 		public ConsecutivePrimeSumTest(ITestOutputHelper output) : base(output)
 		{
 		}
+
+		[Theory]
+		[InlineData(50, 41)]
+		[InlineData(1000, 953)]
+		public void TestSampleData(int upto, int expected)
+		{
+			int actual = _sut.GetConsecutivePrimeSum(upto);
+
+			Assert.Equal(expected, actual);
+		}
 	}
 
 	public class ConsecutivePrimeSum
 	{
+
+		public int GetConsecutivePrimeSum(int upto)
+		{
+			return -1;
+		}
 	}
 }
