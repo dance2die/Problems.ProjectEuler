6e768a54cdfc257ebe3997e690a191f40539bca1
dance2die <dontbothermewithspam@gmail.com>
2016-07-02 11:28:12 -0400

    PE-0052: Added PermutedMultiplesTest

---
+++ Demo.ProjectEuler/Demo.ProjectEuler.Tests/0052/PermutedMultiplesTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Demo.ProjectEuler.Tests.Core;
+using Xunit.Abstractions;
+
+namespace Demo.ProjectEuler.Tests._0052
+{
+	public class PermutedMultiplesTest : BaseTest
+	{
+		private readonly PermutedMultiple _sut = new PermutedMultiple();
+
+		public PermutedMultiplesTest(ITestOutputHelper output) : base(output)
+		{
+		}
+	}
+
+	public class PermutedMultiple
+	{
+	}
+}
