4462c4e5ca28a90bd46e606943dc463a43efc4d5
dance2die <dontbothermewithspam@gmail.com>
2016-07-02 10:36:43 -0400

    PE-Demo-ConsecutivePrimeSum: Still takes 3.8 seconds.

---
+++ Demo.ProjectEuler/ConsecutivePrimeSum/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace ConsecutivePrimeSum
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			Stopwatch stopwatch = new Stopwatch();
+			stopwatch.Start();
+
+			Demo.ProjectEuler.Tests._0050.ConsecutivePrimeSum sut = new Demo.ProjectEuler.Tests._0050.ConsecutivePrimeSum();
+			sut.GetLongestConsecutivePrimeSum(1000000);
+
+			stopwatch.Stop();
+			Console.WriteLine("Ellapsed Time: {0}", stopwatch.Elapsed);
+		}
+	}
+}
