9c2c860f5650c1a8c3849cfb9a1410ad715a0029
dance2die <dontbothermewithspam@gmail.com>
2016-07-03 11:05:47 -0400

    PE-0052: TestDoublingNumberHasSameNumbersWithSampleData passes.

--- Demo.ProjectEuler/Demo.ProjectEuler.Tests/0052/PermutedMultiplesTest.cs
+++ Demo.ProjectEuler/Demo.ProjectEuler.Tests/0052/PermutedMultiplesTest.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Demo.ProjectEuler.Tests.Core;
+using Xunit;
 using Xunit.Abstractions;
 
 namespace Demo.ProjectEuler.Tests._0052
@@ -15,9 +16,32 @@ namespace Demo.ProjectEuler.Tests._0052
 		public PermutedMultiplesTest(ITestOutputHelper output) : base(output)
 		{
 		}
+
+		[Fact]
+		public void TestDoublingNumberHasSameNumbersWithSampleData()
+		{
+			const long input = 125874;
+			const bool expected = true;
+
+			const int multiplyBy = 2;
+			bool actual = _sut.CheckPermutedMultiples(input, multiplyBy);
+
+			Assert.Equal(expected, actual);
+		}
 	}
 
 	public class PermutedMultiple
 	{
+		public bool CheckPermutedMultiples(long input, int multiplyBy)
+		{
+			long multiplied = input * multiplyBy;
+			var multipliedSequence = multiplied.ToString().ToList();
+			multipliedSequence.Sort();
+
+			var inputSequence = input.ToString().ToList();
+			inputSequence.Sort();
+
+			return inputSequence.SequenceEqual(multipliedSequence);
+		}
 	}
 }
