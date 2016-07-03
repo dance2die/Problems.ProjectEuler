5835dcc51556ef646bf2c7da984bde6aca0ee73f
dance2die <dontbothermewithspam@gmail.com>
2016-07-03 11:54:45 -0400

    PE-0053: added CombinatoricSelectionsTest

---
+++ Demo.ProjectEuler/Demo.ProjectEuler.Tests/0053/CombinatoricSelectionsTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Demo.ProjectEuler.Tests.Core;
+using Xunit.Abstractions;
+
+namespace Demo.ProjectEuler.Tests._0053
+{
+	public class CombinatoricSelectionsTest : BaseTest
+	{
+		private readonly CombinatoricSelections _sut = new CombinatoricSelections();
+
+		public CombinatoricSelectionsTest(ITestOutputHelper output) : base(output)
+		{
+		}
+	}
+
+	public class CombinatoricSelections
+	{
+	}
+}
