3fc4f4f0100222012231681b88d4bea70e678e52
dance2die <dontbothermewithspam@gmail.com>
2016-07-04 14:32:23 -0400

    PE-0053: Created a failing test TestNumberOfCominatoricSelectionCount

--- Demo.ProjectEuler/Demo.ProjectEuler.Tests/0053/CombinatoricSelectionsTest.cs
+++ Demo.ProjectEuler/Demo.ProjectEuler.Tests/0053/CombinatoricSelectionsTest.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Demo.ProjectEuler.Tests.Core;
+﻿using Demo.ProjectEuler.Tests.Core;
+using Xunit;
 using Xunit.Abstractions;
 
 namespace Demo.ProjectEuler.Tests._0053
@@ -15,9 +11,24 @@ namespace Demo.ProjectEuler.Tests._0053
 		public CombinatoricSelectionsTest(ITestOutputHelper output) : base(output)
 		{
 		}
+
+		[Fact]
+		public void TestNumberOfCominatoricSelectionCount()
+		{
+			const int input = 12345;
+			const int expected = 10;
+
+			int actual = _sut.GetCombinatoricsSelectionCount(input);
+
+			Assert.Equal(expected, actual);
+		}
 	}
 
 	public class CombinatoricSelections
 	{
+		public int GetCombinatoricsSelectionCount(int input)
+		{
+			return -1;
+		}
 	}
 }
