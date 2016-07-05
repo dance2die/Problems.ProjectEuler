447e6207a90cab569e7e46448bd21f158c51ffd9
dance2die <dontbothermewithspam@gmail.com>
2016-07-05 10:59:19 -0400

    PE-0053: TestNumberOfCominatoricSelectionCount passes.

--- Demo.ProjectEuler/Demo.ProjectEuler.Tests/0053/CombinatoricSelectionsTest.cs
+++ Demo.ProjectEuler/Demo.ProjectEuler.Tests/0053/CombinatoricSelectionsTest.cs
@@ -15,20 +15,46 @@ namespace Demo.ProjectEuler.Tests._0053
 		[Fact]
 		public void TestNumberOfCominatoricSelectionCount()
 		{
-			const int input = 12345;
-			const int expected = 10;
+			int n = 12345.ToString().ToCharArray().Length;
+			const int r = 3;
 
-			int actual = _sut.GetCombinatoricsSelectionCount(input);
+			long actual = _sut.GetCombinatoricsSelectionCount(n, r);
 
+			const long expected = 10;
 			Assert.Equal(expected, actual);
 		}
 	}
 
 	public class CombinatoricSelections
 	{
-		public int GetCombinatoricsSelectionCount(int input)
+		public long GetCombinatoricsSelectionCount(int n, int r)
 		{
-			return -1;
+			long numerator = GetNumerator(n, r);
+			long denominator = GetDenominator(n, r);
+
+			return numerator / denominator;
+		}
+
+		private long GetNumerator(int n, int r)
+		{
+			long result = 1;
+			for (int i = n; i > r; i--)
+			{
+				result *= i;
+			}
+
+			return result;
+		}
+
+		private long GetDenominator(int n, int r)
+		{
+			long result = 1;
+			for (int i = (n - r); i >= 2; i--)
+			{
+				result *= i;
+			}
+
+			return result;
 		}
 	}
 }
