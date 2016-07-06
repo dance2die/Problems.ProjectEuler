9fb130cf947d17d9637209b8adc8141c00ff1f3f
dance2die <dontbothermewithspam@gmail.com>
2016-07-06 19:57:49 -0400

    PE-0055: Added a failing TestLychrelNumber test.

--- Demo.ProjectEuler/Demo.ProjectEuler.Tests/0055/LychrelNumbersTest.cs
+++ Demo.ProjectEuler/Demo.ProjectEuler.Tests/0055/LychrelNumbersTest.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Demo.ProjectEuler.Tests.Core;
+using Xunit;
 using Xunit.Abstractions;
 
 namespace Demo.ProjectEuler.Tests._0055
@@ -15,9 +16,24 @@ namespace Demo.ProjectEuler.Tests._0055
 		public LychrelNumbersTest(ITestOutputHelper output) : base(output)
 		{
 		}
+
+		[Theory]
+		[InlineData(47, true)]
+		[InlineData(349, true)]
+		[InlineData(196, false)]
+		public void TestLychrelNumber(int number, bool expected)
+		{
+			bool actual = _sut.IsLychrelNumber(number);
+
+			Assert.Equal(expected, actual);
+		}
 	}
 
 	public class LychrelNumbers
 	{
+		public bool IsLychrelNumber(int number)
+		{
+			return false;
+		}
 	}
 }
