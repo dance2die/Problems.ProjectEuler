6e1dd29aa13489b7f092227e36ce244e55997a6a
dance2die <dontbothermewithspam@gmail.com>
2016-07-06 19:37:22 -0400

    PE-0055: Created LychrelNumbersTest

---
+++ Demo.ProjectEuler/Demo.ProjectEuler.Tests/0055/LychrelNumbersTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Demo.ProjectEuler.Tests.Core;
+using Xunit.Abstractions;
+
+namespace Demo.ProjectEuler.Tests._0055
+{
+	public class LychrelNumbersTest : BaseTest
+	{
+		private readonly LychrelNumbers _sut = new LychrelNumbers();
+
+		public LychrelNumbersTest(ITestOutputHelper output) : base(output)
+		{
+		}
+	}
+
+	public class LychrelNumbers
+	{
+	}
+}
