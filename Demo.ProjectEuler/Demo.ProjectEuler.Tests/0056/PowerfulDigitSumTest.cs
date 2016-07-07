e6e0bcb18b9e2ca2d88e309961126de47da2143b
dance2die <dontbothermewithspam@gmail.com>
2016-07-07 12:29:07 -0400

    PE-0056: TestSumOfPowerDigitSums passes.

--- Demo.ProjectEuler/Demo.ProjectEuler.Tests/0056/PowerfulDigitSumTest.cs
+++ Demo.ProjectEuler/Demo.ProjectEuler.Tests/0056/PowerfulDigitSumTest.cs
@@ -1,9 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Numerics;
 using Demo.ProjectEuler.Tests.Core;
+using Xunit;
 using Xunit.Abstractions;
 
 namespace Demo.ProjectEuler.Tests._0056
@@ -15,9 +14,35 @@ namespace Demo.ProjectEuler.Tests._0056
 		public PowerfulDigitSumTest(ITestOutputHelper output) : base(output)
 		{
 		}
+
+		[Theory]
+		[InlineData(2, 2, 4)]
+		[InlineData(2, 12, 19)]
+		[InlineData(12, 12, 54)]
+		[InlineData(33, 2, 18)]
+		[InlineData(1, 100, 1)]
+		[InlineData(10, 100, 1)]
+		[InlineData(100, 100, 1)]
+		public void TestSumOfPowerDigitSums(int a, int b, long expected)
+		{
+			long actual = _sut.GetPowerDigitSum(a, b);
+
+			Assert.Equal(expected, actual);
+		}
 	}
 
 	public class PowerfulDigitSum
 	{
+		public long GetPowerDigitSum(int a, int b)
+		{
+			BigInteger powered = BigInteger.One;
+			for (int i = 0; i < b; i++)
+			{
+				powered *= a;
+			}
+
+
+			return powered.ToString().Select(c => Convert.ToInt32(c.ToString())).Sum();
+		}
 	}
 }
