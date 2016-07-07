e65f486b0614077269a8d7f437eb44c4cd973ea8
dance2die <dontbothermewithspam@gmail.com>
2016-07-06 20:49:45 -0400

    PE-0055: Answer accepted but very slow: 14 seconds to run.

--- Demo.ProjectEuler/Demo.ProjectEuler.Tests/0055/LychrelNumbersTest.cs
+++ Demo.ProjectEuler/Demo.ProjectEuler.Tests/0055/LychrelNumbersTest.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using System.Numerics;
 using Demo.ProjectEuler.Core;
 using Demo.ProjectEuler.Tests.Core;
@@ -16,39 +17,60 @@ namespace Demo.ProjectEuler.Tests._0055
 		}
 
 		[Theory]
-		[InlineData(47, true)]
-		[InlineData(349, true)]
-		[InlineData(196, false)]
+		[InlineData(47, false)]
+		[InlineData(349, false)]
+		[InlineData(196, true)]
 		[InlineData(4994, true)]
+		[InlineData(10677, true)]
 		public void TestLychrelNumber(int number, bool expected)
 		{
 			bool actual = _sut.IsLychrelNumber(number);
 
 			Assert.Equal(expected, actual);
 		}
+
+		[Fact]
+		public void ShowResult()
+		{
+			const int upto = 10000;
+
+			int actual = _sut.GetLychrelNumberUpto(upto);
+			_output.WriteLine(actual.ToString());
+		}
 	}
 
 	public class LychrelNumbers
 	{
 		private const int MAX_ITERATION = 50;	// given on the website.
+
+		public int GetLychrelNumberUpto(int upto)
+		{
+			int result = 0;
+			for (int i = 0; i < upto; i++)
+			{
+				if (IsLychrelNumber(i))
+					result++;
+				else
+					Console.WriteLine(i);
+			}
+
+			return result;
+		}
+
 		private readonly Palindrome _palindrome = new Palindrome();
 
 		public bool IsLychrelNumber(BigInteger number)
 		{
 			BigInteger currentNumber = number;
-			if (_palindrome.IsPalindrome(currentNumber.ToString()))
-				return true;
 
 			for (int i = 0; i < MAX_ITERATION; i++)
 			{
-				BigInteger reversedNumber = ReverseNumber(currentNumber);
-				currentNumber += reversedNumber;
-
+				currentNumber += ReverseNumber(currentNumber);
 				if (_palindrome.IsPalindrome(currentNumber.ToString()))
-					return true;
+					return false;
 			}
 
-			return false;
+			return true;
 		}
 
 		private BigInteger ReverseNumber(BigInteger number)
