e86b4f043bb26775691916106409902926090057
dance2die <dontbothermewithspam@gmail.com>
2016-07-06 21:09:45 -0400

    PE-0056: Created PowerfulDigitSumTest

---
+++ Demo.ProjectEuler/Demo.ProjectEuler.Tests/0056/PowerfulDigitSumTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Demo.ProjectEuler.Tests.Core;
+using Xunit.Abstractions;
+
+namespace Demo.ProjectEuler.Tests._0056
+{
+	public class PowerfulDigitSumTest : BaseTest
+	{
+		private readonly PowerfulDigitSum _sut = new PowerfulDigitSum();
+
+		public PowerfulDigitSumTest(ITestOutputHelper output) : base(output)
+		{
+		}
+	}
+
+	public class PowerfulDigitSum
+	{
+	}
+}
---
+++ Demo.ProjectEuler/Demo.ProjectEuler.Tests/0056/README.txt
@@ -0,0 +1,8 @@
+﻿https://projecteuler.net/problem=56
+
+
+Powerful digit sum
+Problem 56
+A googol (10100) is a massive number: one followed by one-hundred zeros; 100100 is almost unimaginably large: one followed by two-hundred zeros. Despite their size, the sum of the digits in each number is only 1.
+
+Considering natural numbers of the form, ab, where a, b < 100, what is the maximum digital sum?
