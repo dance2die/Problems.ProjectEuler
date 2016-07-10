099f9002876ee56955e8c935d55599aa3278f3ee
dance2die <dontbothermewithspam@gmail.com>
2016-07-10 19:44:50 -0400

    PE-0061: Created CyclicalFigurateNumbersTest

---
+++ Demo.ProjectEuler/Demo.ProjectEuler.Tests/0061/CyclicalFigurateNumbersTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Demo.ProjectEuler.Tests.Core;
+using Xunit.Abstractions;
+
+namespace Demo.ProjectEuler.Tests._0061
+{
+	public class CyclicalFigurateNumbersTest : BaseTest
+	{
+		private readonly CyclicalFigurateNumbers _sut = new CyclicalFigurateNumbers();
+
+		public CyclicalFigurateNumbersTest(ITestOutputHelper output) : base(output)
+		{
+		}
+	}
+
+	public class CyclicalFigurateNumbers
+	{
+	}
+}
