f50834e6ce882516e91005c68061af5b04f82057
dance2die <dontbothermewithspam@gmail.com>
2016-07-11 15:45:16 -0400

    PE-0062: created CubicPermutationsTest

---
+++ Demo.ProjectEuler/Demo.ProjectEuler.Tests/0062/CubicPermutationsTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Demo.ProjectEuler.Tests.Core;
+using Xunit.Abstractions;
+
+namespace Demo.ProjectEuler.Tests._0062
+{
+	public class CubicPermutationsTest : BaseTest
+	{
+		private readonly CubicPermutations _sut = new CubicPermutations();
+
+		public CubicPermutationsTest(ITestOutputHelper output) : base(output)
+		{
+		}
+	}
+
+	public class CubicPermutations
+	{
+	}
+}
