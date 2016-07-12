46bbf1dc1d72b162d9dc176e8b1372c0794cc612
dance2die <dontbothermewithspam@gmail.com>
2016-07-11 20:47:53 -0400

    PE-0062: TestSampleDataPermutations passes.

--- Demo.ProjectEuler/Demo.ProjectEuler.Core/MathExt.cs
+++ Demo.ProjectEuler/Demo.ProjectEuler.Core/MathExt.cs
@@ -4,7 +4,7 @@ namespace Demo.ProjectEuler.Core
 {
 	public class MathExt
 	{
-		public double NthRoot(double number, int rootPower)
+		public double NthRoot(double number, double rootPower)
 		{
 			// http://stackoverflow.com/a/18657674/4035
 			return Math.Pow(number, 1.0 / rootPower);
--- Demo.ProjectEuler/Demo.ProjectEuler.Tests/0062/CubicPermutationsTest.cs
+++ Demo.ProjectEuler/Demo.ProjectEuler.Tests/0062/CubicPermutationsTest.cs
@@ -1,9 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Demo.ProjectEuler.Core;
 using Demo.ProjectEuler.Tests.Core;
+using Xunit;
 using Xunit.Abstractions;
 
 namespace Demo.ProjectEuler.Tests._0062
@@ -15,9 +16,59 @@ namespace Demo.ProjectEuler.Tests._0062
 		public CubicPermutationsTest(ITestOutputHelper output) : base(output)
 		{
 		}
+
+		[Theory]
+		[InlineData(345, new [] { 345, 384, 405 })]
+		public void TestSampleDataPermutations(int number, int[] expected)
+		{
+			var actual = _sut.GetCubePermutations(number).Distinct().ToList();
+			actual.Sort();
+
+			Assert.True(expected.SequenceEqual(actual));
+		}
 	}
 
 	public class CubicPermutations
 	{
+		private readonly Permutation _permutation = new Permutation();
+		private readonly MathExt _math = new MathExt();
+
+		public IEnumerable<int> GetCubePermutations(int number)
+		{
+			const double power = 3;	// cube
+			const double precision = 0.000001;
+
+			double cubed = Math.Pow(number, power);
+
+			var cubeSequence = cubed
+				.ToString(CultureInfo.InvariantCulture)
+				.ToCharArray()
+				.Select(c => Convert.ToInt32(c.ToString()))
+				.ToList();
+			var permutations = _permutation.GetPermutations(cubeSequence);
+
+			int index = 0;
+			foreach (IEnumerable<int> sequence in permutations.Distinct())
+			{
+				double doubleValue = ConvertSequenceToDouble(sequence);
+				double rooted = _math.NthRoot(doubleValue, power);
+				double decimalValue = rooted - Math.Truncate(rooted);
+				double difference = 1 - decimalValue;
+
+				if (difference < precision)
+				{
+					Console.WriteLine("index: {0}", index);
+					yield return (int) Math.Round(rooted, 0);
+				}
+
+				index++;
+			}
+		}
+
+		private double ConvertSequenceToDouble(IEnumerable<int> sequence)
+		{
+			string valueText = sequence.Aggregate("", (current, value) => current + value.ToString());
+			return Convert.ToDouble(valueText);
+		}
 	}
 }
