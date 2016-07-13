acca4af6fe5d63614426fe2df185d3f088f0092c
dance2die <dontbothermewithspam@gmail.com>
2016-07-13 10:21:04 -0400

    PE-0062: TestSampleData passes.

--- Demo.ProjectEuler/Demo.ProjectEuler.Tests/0062/CubicPermutationsTest.cs
+++ Demo.ProjectEuler/Demo.ProjectEuler.Tests/0062/CubicPermutationsTest.cs
@@ -53,40 +53,84 @@ namespace Demo.ProjectEuler.Tests._0062
 			Assert.Equal(expected, actual);
 		}
 
-		//[Theory]
-		//[InlineData(41063625, new[] {56623104, 66430125})]
-		//public void TestSampleData(int number, long[] expected)
-		//{
-		//	var actual = _sut.GetCubesWithSamePermutations(number);
+		[Theory]
+		[InlineData(345, new[] {41063625L, 56623104L, 66430125L})]
+		public void TestSampleData(int number, long[] expected)
+		{
+			var actual = _sut.GetCubesWithSamePermutations(number).ToList();
 
-		//	Assert.True(expected.SequenceEqual(actual));
-		//}
+			Assert.True(expected.SequenceEqual(actual));
+		}
 	}
 
 	public class CubicPermutations
 	{
-		//public IEnumerable<long> GetCubesWithSamePermutations(int number)
-		//{
-			
-		//}
+		/// <param name="number">Number for which to cubes with same sequences for</param>
+		/// <remarks>
+		/// To do
+		/// 
+		/// Get the number sequence
+		/// while length of "cubed number" is equal to the given "number",
+		///		Increase number by one and get cube.
+		///		If sequence is the same as cubed number sequence then yield.
+		/// </remarks>
+		public IEnumerable<long> GetCubesWithSamePermutations(int cubeRoot)
+		{
+			const int cubePower = 3;
+			var cubed = (long) Math.Pow(cubeRoot, cubePower);
+
+			var numberList1 = GetNumberList(cubed);
+			for (long i = cubeRoot;; i++)
+			{
+				cubed = (long)Math.Pow(i, cubePower);
+
+				if (i == 384 || i == 405)
+					Console.WriteLine(i);
+
+				var numberList2 = GetNumberList(cubed);
+				if (numberList2.Count > numberList1.Count)
+					yield break;
+
+				if (HasSameNumberSequence(numberList1, numberList2))
+					yield return ConvertSequenceToLong(numberList2);
+			}
+		}
+
+		public bool HasSameNumberSequence<T>(IList<T> list1, IList<T> list2)
+		{
+			var copy1 = list1.ToList();
+			var copy2 = list2.ToList();
+
+			copy1.Sort();
+			copy2.Sort();
+
+			return copy1.SequenceEqual(copy2);
+		}
 
 		public bool HasSameNumberSequence(long number1, long number2)
 		{
-			var list1 = GetSortedNumberList(number1);
-			var list2 = GetSortedNumberList(number2);
+			var list1 = GetNumberList(number1);
+			var list2 = GetNumberList(number2);
 
-			return list1.SequenceEqual(list2);
+			return HasSameNumberSequence(list1, list2);
 		}
 
-		private List<long> GetSortedNumberList(long number)
+		private List<int> GetNumberList(long number)
 		{
-			var list = number
-				.ToString(CultureInfo.InvariantCulture)
-				.ToCharArray()
-				.Select(c => Convert.ToInt64(c.ToString())).ToList();
-			list.Sort();
+			List<int> list = new List<int>();
+			foreach (char c in number.ToString(CultureInfo.InvariantCulture))
+			{
+				list.Add(Convert.ToInt32(c.ToString()));
+			}
+			//list.Sort();
 
 			return list;
+		}
+
+		private long ConvertSequenceToLong(IEnumerable<int> sequence)
+		{
+			string valueText = string.Join("", sequence.Select(i => i.ToString()));
+			return Convert.ToInt64(valueText);
 		}
 
 		//private readonly Permutation _permutation = new Permutation();
@@ -149,10 +193,6 @@ namespace Demo.ProjectEuler.Tests._0062
 		//	return result;
 		//}
 
-		//private double ConvertSequenceToDouble(IEnumerable<int> sequence)
-		//{
-		//	string valueText = string.Join("", sequence.Select(i => i.ToString()));
-		//	return Convert.ToDouble(valueText);
-		//}
+
 	}
 }
