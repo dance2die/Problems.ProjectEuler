f261b16abb47847349464469ee6fca335037280c
dance2die <dontbothermewithspam@gmail.com>
2016-07-14 12:38:04 -0400

    PE-0067: Added MaximumPathSumIITest

---
+++ Demo.ProjectEuler/Demo.ProjectEuler.Tests/0067/MaximumPathSumIITest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Demo.ProjectEuler.Tests.Core;
+using Xunit.Abstractions;
+
+namespace Demo.ProjectEuler.Tests._0067
+{
+	public class MaximumPathSumIITest : BaseTest
+	{
+		private readonly MaximumPathSumII _sut = new MaximumPathSumII();
+
+		public MaximumPathSumIITest(ITestOutputHelper output) : base(output)
+		{
+		}
+	}
+
+	public class MaximumPathSumII
+	{
+	}
+}
