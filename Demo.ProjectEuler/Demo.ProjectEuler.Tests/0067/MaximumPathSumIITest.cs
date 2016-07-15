bd24108f7fb183ec14a2d51e544d21ee01e4d827
dance2die <dontbothermewithspam@gmail.com>
2016-07-15 07:25:45 -0400

    PE-0062: added

---
+++ Demo.ProjectEuler/Demo.ProjectEuler.Tests/0067/MaximumPathSumIITest.cs
@@ -0,0 +1,37 @@
+﻿using System.IO;
+using System.Numerics;
+using Demo.ProjectEuler.Tests.Core;
+using Demo.ProjectEuler.Tests._0018;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Demo.ProjectEuler.Tests._0067
+{
+	public class MaximumPathSumIITest : BaseTest
+	{
+		private readonly MaximumPathSumII _sut = new MaximumPathSumII();
+
+		public MaximumPathSumIITest(ITestOutputHelper output) : base(output)
+		{
+		}
+
+		[Fact]
+		public void ShowResult()
+		{
+			string input = File.ReadAllText("./0067/p067_triangle.txt");
+
+			BigInteger actual = _sut.GetMximumPathSum(input);
+			_output.WriteLine(actual.ToString());
+		}
+	}
+
+	public class MaximumPathSumII
+	{
+		private readonly MaximumPathSum _maximumPathSum = new MaximumPathSum();
+
+		public BigInteger GetMximumPathSum(string input)
+		{
+			return _maximumPathSum.GetMximumPathSum(input);
+		}
+	}
+}
