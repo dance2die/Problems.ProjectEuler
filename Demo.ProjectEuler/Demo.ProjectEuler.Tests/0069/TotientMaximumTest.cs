1169ad10df3ae2e57549bcd17a7df1c907d34396
dance2die <dontbothermewithspam@gmail.com>
2016-07-15 07:21:18 -0400

    PE-0069: added TotientMaximumTest

---
+++ Demo.ProjectEuler/Demo.ProjectEuler.Tests/0069/TotientMaximumTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Demo.ProjectEuler.Tests.Core;
+using Xunit.Abstractions;
+
+namespace Demo.ProjectEuler.Tests._0069
+{
+	public class TotientMaximumTest : BaseTest
+	{
+		private readonly TotientMaximum _sut = new TotientMaximum();
+
+		public TotientMaximumTest(ITestOutputHelper output) : base(output)
+		{
+		}
+	}
+
+	public class TotientMaximum
+	{
+	}
+}
