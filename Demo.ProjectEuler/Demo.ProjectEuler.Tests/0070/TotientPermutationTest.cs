83342fd1f455bf8714f2f2fef76310438cd550ad
dance2die <dontbothermewithspam@gmail.com>
2016-07-20 15:32:50 -0400

    PE-0070: created TotientPermutationTest

---
+++ Demo.ProjectEuler/Demo.ProjectEuler.Tests/0070/TotientPermutationTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Demo.ProjectEuler.Tests.Core;
+using Xunit.Abstractions;
+
+namespace Demo.ProjectEuler.Tests._0070
+{
+    public class TotientPermutationTest : BaseTest
+    {
+        private readonly TotientPermutation _sut = new TotientPermutation();
+
+        public TotientPermutationTest(ITestOutputHelper output) : base(output)
+        {
+        }
+    }
+
+    public class TotientPermutation
+    {
+    }
+}
