8a970e80cbdd764faba2d4068df4d8d42ff46110
dance2die <dontbothermewithspam@gmail.com>
2016-07-20 16:33:45 -0400

    PE-LibraryTest-PermutationTest: Created PermutationTest to test IsPermutation

---
+++ Demo.ProjectEuler/Demo.ProjectEuler.Tests/LibraryTests/PermutationTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Demo.ProjectEuler.Core;
+using Demo.ProjectEuler.Tests.Core;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Demo.ProjectEuler.Tests.LibraryTests
+{
+    public class PermutationTest : BaseTest
+    {
+        private readonly Permutation _sut = new Permutation();
+
+        public PermutationTest(ITestOutputHelper output) : base(output)
+        {
+        }
+    }
+}
