908ac38e6142015928be9f4c3b7558a07d6dc578
dance2die <dontbothermewithspam@gmail.com>
2016-07-22 10:37:39 -0400

    PE-0071: OrderedFractionsTest created

---
+++ Demo.ProjectEuler/Demo.ProjectEuler.Tests/0071/OrderedFractionsTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Demo.ProjectEuler.Tests.Core;
+using Xunit.Abstractions;
+
+namespace Demo.ProjectEuler.Tests._0071
+{
+	public class OrderedFractionsTest : BaseTest
+	{
+		private readonly OrderedFractions _sut = new OrderedFractions();
+
+		public OrderedFractionsTest(ITestOutputHelper output) : base(output)
+		{
+		}
+	}
+
+	public class OrderedFractions
+	{
+	}
+}
