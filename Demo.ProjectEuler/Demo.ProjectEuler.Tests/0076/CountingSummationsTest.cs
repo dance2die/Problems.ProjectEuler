bfbd816e060074dccf56f7234cba237318c1ff54
dance2die <dontbothermewithspam@gmail.com>
2016-07-28 19:53:41 -0400

    PE-0076: Added CountingSummationsTest

---
+++ Demo.ProjectEuler/Demo.ProjectEuler.Tests/0076/CountingSummationsTest.cs
@@ -0,0 +1,18 @@
+﻿using Demo.ProjectEuler.Tests.Core;
+using Xunit.Abstractions;
+
+namespace Demo.ProjectEuler.Tests._0076
+{
+	public class CountingSummationsTest : BaseTest
+	{
+		private readonly CountingSummations _sut = new CountingSummations();
+
+		public CountingSummationsTest(ITestOutputHelper output) : base(output)
+		{
+		}
+	}
+
+	public class CountingSummations
+	{
+	}
+}
