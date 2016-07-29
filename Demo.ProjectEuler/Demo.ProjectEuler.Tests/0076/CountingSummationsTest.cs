089f4c1bf9cd927939b57b884a58b9bb5be814bc
dance2die <dontbothermewithspam@gmail.com>
2016-07-28 20:24:56 -0400

    PE-0076: Permutation counting will not work. Too slow.

--- Demo.ProjectEuler/Demo.ProjectEuler.Tests/0076/CountingSummationsTest.cs
+++ Demo.ProjectEuler/Demo.ProjectEuler.Tests/0076/CountingSummationsTest.cs
@@ -1,4 +1,7 @@
-﻿using Demo.ProjectEuler.Tests.Core;
+﻿using System.Linq;
+using Demo.ProjectEuler.Core;
+using Demo.ProjectEuler.Tests.Core;
+using Xunit;
 using Xunit.Abstractions;
 
 namespace Demo.ProjectEuler.Tests._0076
@@ -10,9 +13,28 @@ namespace Demo.ProjectEuler.Tests._0076
 		public CountingSummationsTest(ITestOutputHelper output) : base(output)
 		{
 		}
+
+		[Theory]
+		[InlineData(2, 1)]
+		[InlineData(3, 2)]
+		[InlineData(5, 6)]
+		[InlineData(100, 100)]
+		public void TestSummationCounting(int n, int expected)
+		{
+			int actual = _sut.GetSummationCount(n);
+
+			Assert.Equal(expected, actual);
+		}
 	}
 
 	public class CountingSummations
 	{
+		private readonly Permutation _permutation = new Permutation();
+
+		public int GetSummationCount(int n)
+		{
+			var permutations = _permutation.GetPermutations(Enumerable.Range(1, n).ToList());
+			return permutations.Count();
+		}
 	}
 }
