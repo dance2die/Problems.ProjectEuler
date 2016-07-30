08ea19765469e56a2543f69084453a7f5e7eb685
dance2die <dontbothermewithspam@gmail.com>
2016-07-30 11:44:34 -0400

    BigInteger test failure.

---
+++ Demo.ProjectEuler/BigIntegerTest/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BigIntegerTest
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			BigInteger two = 2 * BigInteger.Pow(10, 200);
+			BigInteger squareRoot = GetBigIntegerSquareRoot(two);
+			Console.WriteLine(squareRoot);
+
+			var sum = squareRoot.ToString().ToCharArray().Select(c => Convert.ToInt32(c.ToString())).Take(100).Sum();
+			Console.WriteLine(sum);
+		}
+
+		private static BigInteger GetBigIntegerSquareRoot(BigInteger value)
+		{
+			return new BigInteger(Math.Exp(BigInteger.Log(value) / 2));
+		}
+	}
+}
