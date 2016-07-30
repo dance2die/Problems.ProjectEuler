d5b271abd927ced4a0786191be5a21476acba553
dance2die <dontbothermewithspam@gmail.com>
2016-07-30 11:48:21 -0400

    PE-0081: created "PathSumTwoWaysTest"

---
+++ Demo.ProjectEuler/Demo.ProjectEuler.Tests/0081/PathSumTwoWaysTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Demo.ProjectEuler.Tests.Core;
+using Xunit.Abstractions;
+
+namespace Demo.ProjectEuler.Tests._0081
+{
+	public class PathSumTwoWaysTest : BaseTest
+	{
+		private readonly PathSumTwoWays _sut = new PathSumTwoWays();
+
+		public PathSumTwoWaysTest(ITestOutputHelper output) : base(output)
+		{
+		}
+	}
+
+	public class PathSumTwoWays
+	{
+	}
+}
