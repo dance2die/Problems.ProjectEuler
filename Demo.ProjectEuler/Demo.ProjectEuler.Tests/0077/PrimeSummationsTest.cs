890c69f6a2c8e9c9bdbd6278f0fe58906dbd8030
dance2die <dontbothermewithspam@gmail.com>
2016-07-30 09:08:49 -0400

    PE-0077: Created PrimeSummationsTest

---
+++ Demo.ProjectEuler/Demo.ProjectEuler.Tests/0077/PrimeSummationsTest.cs
@@ -0,0 +1,18 @@
+﻿using Demo.ProjectEuler.Tests.Core;
+using Xunit.Abstractions;
+
+namespace Demo.ProjectEuler.Tests._0077
+{
+	public class PrimeSummationsTest : BaseTest
+	{
+		private readonly PrimeSummations _sut = new PrimeSummations();
+
+		public PrimeSummationsTest(ITestOutputHelper output) : base(output)
+		{
+		}
+	}
+
+	public class PrimeSummations
+	{
+	}
+}
