fb6cdf3bd9e8b5ef62864d7156688a89ee060b87
dance2die <dontbothermewithspam@gmail.com>
2016-08-03 14:47:17 -0400

    PE-0087: Added PrimePowerTriplesTest

---
+++ Demo.ProjectEuler/Demo.ProjectEuler.Tests/0087/PrimePowerTriplesTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Demo.ProjectEuler.Tests.Core;
+using Xunit.Abstractions;
+
+namespace Demo.ProjectEuler.Tests._0087
+{
+	public class PrimePowerTriplesTest : BaseTest
+	{
+		private readonly PrimePowerTriples _sut = new PrimePowerTriples();
+
+		public PrimePowerTriplesTest(ITestOutputHelper output) : base(output)
+		{
+		}
+	}
+
+	public class PrimePowerTriples
+	{
+	}
+}
