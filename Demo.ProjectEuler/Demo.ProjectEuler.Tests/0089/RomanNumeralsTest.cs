fb1aca6d243af268305c6499a9711936d5210244
dance2die <dontbothermewithspam@gmail.com>
2016-08-05 11:02:25 -0400

    PE-0089: created RomanNumeralsTest

---
+++ Demo.ProjectEuler/Demo.ProjectEuler.Tests/0089/RomanNumeralsTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Demo.ProjectEuler.Tests.Core;
+using Xunit.Abstractions;
+
+namespace Demo.ProjectEuler.Tests._0089
+{
+	public class RomanNumeralsTest : BaseTest
+	{
+		private readonly RomanNumerals _sut = new RomanNumerals();
+
+		public RomanNumeralsTest(ITestOutputHelper output) : base(output)
+		{
+		}
+	}
+
+	public class RomanNumerals
+	{
+	}
+}
