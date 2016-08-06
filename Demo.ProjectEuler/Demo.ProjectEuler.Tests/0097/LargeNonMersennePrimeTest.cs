89f1bc37a944844f2a549a44409a9c27c7fbd661
dance2die <dontbothermewithspam@gmail.com>
2016-08-06 15:11:58 -0400

    PE-0097: Created LargeNonMersennePrimeTest

---
+++ Demo.ProjectEuler/Demo.ProjectEuler.Tests/0097/LargeNonMersennePrimeTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Demo.ProjectEuler.Tests.Core;
+using Xunit.Abstractions;
+
+namespace Demo.ProjectEuler.Tests._0097
+{
+	public class LargeNonMersennePrimeTest : BaseTest
+	{
+		private readonly LargeNonMersennePrime _sut = new LargeNonMersennePrime();
+
+		public LargeNonMersennePrimeTest(ITestOutputHelper output) : base(output)
+		{
+		}
+	}
+
+	public class LargeNonMersennePrime
+	{
+	}
+}
