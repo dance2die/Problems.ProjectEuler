7ede169e1c373c443c6e4c6bba4ae0a37893d9c6
dance2die <dontbothermewithspam@gmail.com>
2016-08-06 09:55:34 -0400

    PE-0092: Created SquareDigitChainsTest

---
+++ Demo.ProjectEuler/Demo.ProjectEuler.Tests/0092/SquareDigitChainsTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Demo.ProjectEuler.Tests.Core;
+using Xunit.Abstractions;
+
+namespace Demo.ProjectEuler.Tests._0092
+{
+	public class SquareDigitChainsTest : BaseTest
+	{
+		private readonly SquareDigitChains _sut = new SquareDigitChains();
+
+		public SquareDigitChainsTest(ITestOutputHelper output) : base(output)
+		{
+		}
+	}
+
+	public class SquareDigitChains
+	{
+	}
+}
