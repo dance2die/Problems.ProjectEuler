7bdcc88ab16e03a84f4b2eb7787400efb3651adb
dance2die <dontbothermewithspam@gmail.com>
2016-08-06 14:01:33 -0400

    PE-0092: Happy Number check passes.

--- Demo.ProjectEuler/Demo.ProjectEuler.Tests/0092/SquareDigitChainsTest.cs
+++ Demo.ProjectEuler/Demo.ProjectEuler.Tests/0092/SquareDigitChainsTest.cs
@@ -1,9 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Demo.ProjectEuler.Tests.Core;
+using Xunit;
 using Xunit.Abstractions;
 
 namespace Demo.ProjectEuler.Tests._0092
@@ -15,9 +14,52 @@ namespace Demo.ProjectEuler.Tests._0092
 		public SquareDigitChainsTest(ITestOutputHelper output) : base(output)
 		{
 		}
+
+		[Theory]
+		[InlineData(1, true)]
+		[InlineData(2, false)]
+		[InlineData(7, true)]
+		[InlineData(10, true)]
+		[InlineData(13, true)]
+		[InlineData(20, false)]
+		[InlineData(269, false)]
+		[InlineData(339, false)]
+		[InlineData(469, true)]
+		[InlineData(900, false)]
+		[InlineData(1000, true)]
+		public void TestHappyNumberCheck(long n, bool expected)
+		{
+			bool actual = _sut.IsHappyNumber(n);
+
+			Assert.Equal(expected, actual);
+		}
 	}
 
 	public class SquareDigitChains
 	{
+		/// <summary>
+		/// Check happy number <see cref="https://en.wikipedia.org/wiki/Happy_number"/>
+		/// </summary>
+		public bool IsHappyNumber(long n)
+		{
+			HashSet<long> seenNumbers = new HashSet<long>();
+			while (n > 1 && !seenNumbers.Contains(n))
+			{
+				seenNumbers.Add(n);
+				n = MapSquare(n);
+			}
+
+			return n == 1;
+		}
+
+		private long MapSquare(long n)
+		{
+			return n.ToString().ToCharArray().Select(c => Square(Convert.ToInt64(c.ToString()))).Sum();
+		}
+
+		private long Square(long n)
+		{
+			return n * n;
+		}
 	}
 }
