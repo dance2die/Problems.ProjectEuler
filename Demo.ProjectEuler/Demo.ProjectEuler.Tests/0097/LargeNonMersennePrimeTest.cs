8bda92ac9edb96a8ed5fbc0876e9e57c712997b0
dance2die <dontbothermewithspam@gmail.com>
2016-08-06 15:32:55 -0400

    PE-0097: took 10+ MINUTES!

--- Demo.ProjectEuler/Demo.ProjectEuler.Tests/0097/LargeNonMersennePrimeTest.cs
+++ Demo.ProjectEuler/Demo.ProjectEuler.Tests/0097/LargeNonMersennePrimeTest.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 using Demo.ProjectEuler.Tests.Core;
+using Xunit;
 using Xunit.Abstractions;
 
 namespace Demo.ProjectEuler.Tests._0097
@@ -14,6 +16,22 @@ namespace Demo.ProjectEuler.Tests._0097
 
 		public LargeNonMersennePrimeTest(ITestOutputHelper output) : base(output)
 		{
+		}
+
+		[Fact]
+		public void ShowResult()
+		{
+			const int power = 7830457;
+
+			int left = 28433;
+			BigInteger right = BigInteger.Pow(2, power);
+
+			BigInteger result = left * right + 1;
+
+			string resultString = result.ToString();
+			_output.WriteLine(resultString.Substring(resultString.Length - 10));
+
+			// output 8739992577
 		}
 	}
 
