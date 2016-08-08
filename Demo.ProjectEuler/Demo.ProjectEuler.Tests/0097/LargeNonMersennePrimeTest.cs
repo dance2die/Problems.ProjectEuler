eb3bacaddde4daa5e6c9faffa09d3ca1091d0a62
dance2die <dontbothermewithspam@gmail.com>
2016-08-08 09:42:07 -0400

    PE-0097: Optimized runs at 2 seconds.

--- Demo.ProjectEuler/Demo.ProjectEuler.Tests/0097/LargeNonMersennePrimeTest.cs
+++ Demo.ProjectEuler/Demo.ProjectEuler.Tests/0097/LargeNonMersennePrimeTest.cs
@@ -1,4 +1,5 @@
-﻿using System.Numerics;
+﻿using System;
+using System.Numerics;
 using Demo.ProjectEuler.Tests.Core;
 using Xunit;
 using Xunit.Abstractions;
@@ -32,9 +33,9 @@ namespace Demo.ProjectEuler.Tests._0097
 		[Fact]
 		public void ShowResultOptimized()
 		{
-			const long expected = 8739992577;
+			const string expected = "8739992577";
 
-			long actual = _sut.GetLast10Digits();
+			string actual = _sut.GetLast10Digits();
 
 			Assert.Equal(expected, actual);
 		}
@@ -42,9 +43,25 @@ namespace Demo.ProjectEuler.Tests._0097
 
 	public class LargeNonMersennePrime
 	{
-		public long GetLast10Digits()
+		public string GetLast10Digits()
 		{
-			return -1;
+			const long power = 7830456;	// one less because we are starting with 2.
+			const long multiplier = 28433;
+
+			long last10Digits = 2;
+			for (int i = 1; i <= power; i++)
+			{
+				long powered = last10Digits * 2;
+				string poweredString = powered.ToString();
+				last10Digits = poweredString.Length > 10 
+					? Convert.ToInt64(poweredString.Substring(poweredString.Length - 10))
+					: powered;
+			}
+
+			long output = last10Digits * multiplier + 1;
+			string outputString = output.ToString();
+
+			return outputString.Substring(outputString.Length - 10);
 		}
 	}
 }
