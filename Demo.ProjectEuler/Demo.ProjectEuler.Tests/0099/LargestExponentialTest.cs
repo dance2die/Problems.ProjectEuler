f19812a1029f5c834f665a01ddbd5f24681822ed
dance2die <dontbothermewithspam@gmail.com>
2016-08-09 07:17:05 -0400

    PE-0099: Optimized solving against sample data using number theory.

--- Demo.ProjectEuler/Demo.ProjectEuler.Tests/0099/LargestExponentialTest.cs
+++ Demo.ProjectEuler/Demo.ProjectEuler.Tests/0099/LargestExponentialTest.cs
@@ -1,9 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Numerics;
 using Demo.ProjectEuler.Tests.Core;
+using Xunit;
 using Xunit.Abstractions;
 
 namespace Demo.ProjectEuler.Tests._0099
@@ -15,9 +13,51 @@ namespace Demo.ProjectEuler.Tests._0099
 		public LargestExponentialTest(ITestOutputHelper output) : base(output)
 		{
 		}
+
+		[Fact]
+		public void TestSampleData()
+		{
+			const long base1 = 632382;
+			const int exponent1 = 518061;
+			const long base2 = 519432;
+			const int exponent2 = 525806;
+
+			Tuple<long, int> leftValue = new Tuple<long, int>(base1, exponent1);
+			Tuple<long, int> rightValue = new Tuple<long, int>(base2, exponent2);
+
+			Tuple<long, int> max = _sut.CompareValues(leftValue, rightValue);
+
+			Assert.Equal(leftValue, max);
+
+			//BigInteger value1 = _sut.GetPoweredValue(base1, exponent1);
+			//BigInteger value2 = _sut.GetPoweredValue(base2, exponent2);
+
+			//Assert.True(value1 > value2);
+
+		}
 	}
 
 	public class LargestExponential
 	{
+		public Tuple<long, int> CompareValues(Tuple<long, int> leftValue, Tuple<long, int> rightValue)
+		{
+			double log1 = Math.Log(leftValue.Item1);
+			double log2 = Math.Log(rightValue.Item1);
+
+			BigInteger poweredValue1 = GetPoweredValue(log1, leftValue.Item2);
+			BigInteger poweredValue2 = GetPoweredValue(log2, rightValue.Item2);
+
+			Console.WriteLine(poweredValue1);
+			Console.WriteLine(poweredValue2);
+
+			return poweredValue1 > poweredValue2 ? leftValue : rightValue;
+		}
+
+		public BigInteger GetPoweredValue(double baseValue, int exponent)
+		{
+			//return BigInteger.Pow(new BigInteger(baseValue), exponent);
+			// http://math.stackexchange.com/questions/8308/working-with-large-exponents
+			return new BigInteger(exponent * baseValue);
+		}
 	}
 }
