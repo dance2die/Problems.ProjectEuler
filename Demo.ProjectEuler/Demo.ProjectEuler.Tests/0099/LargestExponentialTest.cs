59cbcb1601b2f5cff3eea08bd0ea89aadbf88fa7
dance2die <dontbothermewithspam@gmail.com>
2016-08-09 06:44:53 -0400

    PE-0099: Created LargestExponentialTest

---
+++ Demo.ProjectEuler/Demo.ProjectEuler.Tests/0099/LargestExponentialTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Demo.ProjectEuler.Tests.Core;
+using Xunit.Abstractions;
+
+namespace Demo.ProjectEuler.Tests._0099
+{
+	public class LargestExponentialTest : BaseTest
+	{
+		private readonly LargestExponential _sut = new LargestExponential();
+
+		public LargestExponentialTest(ITestOutputHelper output) : base(output)
+		{
+		}
+	}
+
+	public class LargestExponential
+	{
+	}
+}
