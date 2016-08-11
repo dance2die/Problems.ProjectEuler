751308bf1bddee311a50873d91b47cb2fd313313
dance2die <dontbothermewithspam@gmail.com>
2016-08-11 09:13:19 -0400

    PE-0102: created TriangleContainmentTest

---
+++ Demo.ProjectEuler/Demo.ProjectEuler.Tests/0102/TriangleContainmentTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Demo.ProjectEuler.Tests.Core;
+using Xunit.Abstractions;
+
+namespace Demo.ProjectEuler.Tests._0102
+{
+	public class TriangleContainmentTest : BaseTest
+	{
+		private readonly TriangleContainment _sut = new TriangleContainment();
+
+		public TriangleContainmentTest(ITestOutputHelper output) : base(output)
+		{
+		}
+	}
+
+	public class TriangleContainment
+	{
+	}
+}
