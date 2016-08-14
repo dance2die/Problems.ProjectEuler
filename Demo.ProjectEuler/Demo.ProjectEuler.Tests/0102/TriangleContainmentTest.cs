c15ffff56bae839e480beb1733044b6de4ac0227
dance2die <dontbothermewithspam@gmail.com>
2016-08-14 11:33:41 -0400

    PE-0102: TestSampleData passes.

--- Demo.ProjectEuler/Demo.ProjectEuler.Tests/0102/TriangleContainmentTest.cs
+++ Demo.ProjectEuler/Demo.ProjectEuler.Tests/0102/TriangleContainmentTest.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Demo.ProjectEuler.Tests.Core;
 using Xunit;
 using Xunit.Abstractions;
@@ -34,12 +30,27 @@ namespace Demo.ProjectEuler.Tests._0102
 		/// Find if the triangle specified by the given points contains origin or not
 		/// using algorithm found here. <see cref="http://stackoverflow.com/a/2049593/4035"/>
 		/// </summary>
+		/// <remarks>
+		/// <see cref="http://stackoverflow.com/questions/2049582/how-to-determine-if-a-point-is-in-a-2d-triangle"/>
+		/// </remarks>
 		public bool ContainsOrigin(int[] points)
 		{
 			if (points.Length != 6)
 				throw new ArgumentException("There should be 6 numbers!", nameof(points));
 
-			return false;
+			const int originX = 0;
+			const int originY = 0;
+
+			var b1 = sign(originX, originY, points[0], points[1], points[2], points[3]) < 0.0f;
+			var b2 = sign(originX, originY, points[2], points[3], points[4], points[5]) < 0.0f;
+			var b3 = sign(originX, originY, points[4], points[5], points[0], points[1]) < 0.0f;
+
+			return ((b1 == b2) && (b2 == b3));
+		}
+
+		private float sign(int x1, int y1, int x2, int y2, int x3, int y3)
+		{
+			return (x1 - x3) * (y2 - y3) - (x2 - x3) * (y1 - y3);
 		}
 	}
 }
