bd8647d117fe4149e345a806075b9cec6cb3ca54
dance2die <dontbothermewithspam@gmail.com>
2016-08-15 09:29:53 -0400

    PE-0112: Created BouncyNumbersTest

---
+++ Demo.ProjectEuler/Demo.ProjectEuler.Tests/0112/BouncyNumbersTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Demo.ProjectEuler.Tests.Core;
+using Xunit.Abstractions;
+
+namespace Demo.ProjectEuler.Tests._0112
+{
+	public class BouncyNumbersTest : BaseTest
+	{
+		private readonly BouncyNumbers _sut = new BouncyNumbers();
+
+		public BouncyNumbersTest(ITestOutputHelper output) : base(output)
+		{
+		}
+	}
+
+	public class BouncyNumbers
+	{
+	}
+}
