4f69aa8b2d4aa76f1135220fe23068f1a52e4323
dance2die <dontbothermewithspam@gmail.com>
2016-08-16 10:21:25 -0400

    PE-LibraryTest-NumberUtilTest: ToSequence returns numbers in reverse order.

--- Demo.ProjectEuler/Demo.ProjectEuler.Tests/0112/BouncyNumbersTest.cs
+++ Demo.ProjectEuler/Demo.ProjectEuler.Tests/0112/BouncyNumbersTest.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Demo.ProjectEuler.Tests.Core;
+using Xunit;
 using Xunit.Abstractions;
 
 namespace Demo.ProjectEuler.Tests._0112
@@ -15,9 +16,32 @@ namespace Demo.ProjectEuler.Tests._0112
 		public BouncyNumbersTest(ITestOutputHelper output) : base(output)
 		{
 		}
+
+		[Theory]
+		[InlineData(134468, true)]
+		[InlineData(12345, true)]
+		[InlineData(12345, true)]
+		[InlineData(4321, false)]
+		[InlineData(88778, false)]
+		[InlineData(1, false)]
+		[InlineData(45, false)]
+		[InlineData(99, false)]
+		public void TestIncreasingNumber(int input, bool expected)
+		{
+			bool actual = _sut.IsIncreasingNumber(input);
+
+			Assert.Equal(expected, actual);
+		}
 	}
 
 	public class BouncyNumbers
 	{
+		public bool IsIncreasingNumber(int input)
+		{
+			return false;
+		}
+
+
+
 	}
 }
---
+++ Demo.ProjectEuler/Demo.ProjectEuler.Tests/LibraryTests/NumberUtilTest.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Demo.ProjectEuler.Tests.Core;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Demo.ProjectEuler.Tests.LibraryTests
+{
+	public class NumberUtilTest : BaseTest
+	{
+		private readonly NumberUtil _sut = new NumberUtil();
+
+		public NumberUtilTest(ITestOutputHelper output) : base(output)
+		{
+		}
+
+		[Theory]
+		[InlineData(123, new [] {1,2,3})]
+		[InlineData(1, new [] {1})]
+		[InlineData(2, new [] {2})]
+		[InlineData(2234, new [] {2,2,3,4})]
+		[InlineData(1234567890, new [] {1,2,3,4,5,6,7,8,9,0})]
+		[InlineData(9876, new [] {9,8,7,6})]
+		public void TestNumberToSequence(long input, int[] expected)
+		{
+			int[] actual = _sut.ToSequence(input).ToArray();
+
+			Assert.True(expected.SequenceEqual(actual));
+		}
+	}
+
+	public class NumberUtil
+	{
+		public IEnumerable<int> ToSequence(long number)
+		{
+			while (number > 0)
+			{
+				int digit = (int)number % 10;
+				yield return digit;
+
+				number /= 10;
+			}
+		}
+	}
+}
