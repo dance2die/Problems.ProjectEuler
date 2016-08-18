49214db5704db70dac0aa559cfb13a02bc1bf99f
dance2die <dontbothermewithspam@gmail.com>
2016-08-18 09:25:51 -0400

    PE-0112: TestDecreasingNumber passes.

--- Demo.ProjectEuler/Demo.ProjectEuler.Tests/0112/BouncyNumbersTest.cs
+++ Demo.ProjectEuler/Demo.ProjectEuler.Tests/0112/BouncyNumbersTest.cs
@@ -30,11 +30,47 @@ namespace Demo.ProjectEuler.Tests._0112
 
 			Assert.Equal(expected, actual);
 		}
+
+		[Theory]
+		[InlineData(134468, false)]
+		[InlineData(12345, false)]
+		[InlineData(12345, false)]
+		[InlineData(4321, true)]
+		[InlineData(88778, false)]
+		[InlineData(1, false)]
+		[InlineData(45, false)]
+		[InlineData(99, false)]
+		[InlineData(98765, true)]
+		[InlineData(8531, true)]
+		public void TestDecreasingNumber(int input, bool expected)
+		{
+			bool actual = _sut.IsDecreasingNumber(input);
+
+			Assert.Equal(expected, actual);
+		}
 	}
 
 	public class BouncyNumbers
 	{
 		private readonly NumberUtil _numberUtil = new NumberUtil();
+
+		public bool IsDecreasingNumber(int input)
+		{
+			if (input < 100) return false;  // by definition
+
+			IEnumerable<int> numbers = _numberUtil.ToReverseSequence(input).Reverse();
+			int previousNumber = 9;
+
+			foreach (int number in numbers)
+			{
+				if (previousNumber < number)
+					return false;
+
+				previousNumber = number;
+			}
+
+			return true;
+		}
 
 		public bool IsIncreasingNumber(int input)
 		{
@@ -53,8 +89,5 @@ namespace Demo.ProjectEuler.Tests._0112
 
 			return true;
 		}
-
-
-
 	}
 }
