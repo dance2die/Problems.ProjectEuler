5a6daa5a0cfa3687c1cff4ea51835b78b51feb39
dance2die <dontbothermewithspam@gmail.com>
2016-08-19 20:51:04 -0400

    PE-0112: Refactored.

--- Demo.ProjectEuler/Demo.ProjectEuler.Tests/0112/BouncyNumbersTest.cs
+++ Demo.ProjectEuler/Demo.ProjectEuler.Tests/0112/BouncyNumbersTest.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using Demo.ProjectEuler.Core;
 using Demo.ProjectEuler.Tests.Core;
 using Xunit;
 using Xunit.Abstractions;
@@ -123,97 +120,6 @@ namespace Demo.ProjectEuler.Tests._0112
 
 			const int expected = 1587000;
 			Assert.Equal(expected, actual);
-		}
-	}
-
-	public class BouncyNumbers
-	{
-		private readonly NumberUtil _numberUtil = new NumberUtil();
-
-		public int GetBouncyNumberPercentageUpto(int uptoPercentage)
-		{
-			int bouncyNumberCountTotal = 0;
-			int from = 1;
-
-			do
-			{
-				int bouncyNumberCount = GetBouncyNumberCountBetween(from, 1);
-				bouncyNumberCountTotal += bouncyNumberCount;
-				if (bouncyNumberCountTotal*100/from == 99)
-					return from;
-
-				from++;
-			} while (true);
-		}
-
-		public int GetBouncyNumberPercentage(int upto)
-		{
-			return GetBouncyNumberCount(upto) * 100 / upto;
-		}
-
-		public int GetBouncyNumberCountBetween(int from, int count)
-		{
-			int bouncyNumberCount = 0;
-			var numbers = Enumerable.Range(from, count);
-			foreach (var number in numbers)
-			{
-				if (IsBouncyNumber(number))
-					bouncyNumberCount++;
-			}
-
-			return bouncyNumberCount;
-		}
-
-		public int GetBouncyNumberCount(int upto)
-		{
-			int bouncyNumberCount = 0;
-			var numbers = Enumerable.Range(1, upto).ToList();
-			foreach (var number in numbers)
-			{
-				if (IsBouncyNumber(number))
-					bouncyNumberCount++;
-			}
-
-			return bouncyNumberCount;
-		}
-
-		public bool IsBouncyNumber(int input)
-		{
-			if (input < 100) return false;	// by definition
-
-			return !IsIncreasingNumber(input) && !IsDecreasingNumber(input);
-		}
-
-		public bool IsDecreasingNumber(int input)
-		{
-			IEnumerable<int> numbers = _numberUtil.ToReverseSequence(input).Reverse();
-			int previousNumber = 9;
-
-			foreach (int number in numbers)
-			{
-				if (previousNumber < number)
-					return false;
-
-				previousNumber = number;
-			}
-
-			return true;
-		}
-
-		public bool IsIncreasingNumber(int input)
-		{
-			IEnumerable<int> numbers = _numberUtil.ToReverseSequence(input).Reverse();
-			int previousNumber = 0;
-
-			foreach (int number in numbers)
-			{
-				if (previousNumber > number)
-					return false;
-
-				previousNumber = number;
-			}
-
-			return true;
 		}
 	}
 }
