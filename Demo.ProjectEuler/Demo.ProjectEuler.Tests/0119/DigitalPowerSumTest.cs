37e67f96195214800c5791c3238afb00f8b545c1
dance2die <dontbothermewithspam@gmail.com>
2016-08-21 08:54:48 -0400

    PE-0119: Created DigitalPowerSumTest

---
+++ Demo.ProjectEuler/Demo.ProjectEuler.Tests/0119/DigitalPowerSumTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Demo.ProjectEuler.Tests.Core;
+using Xunit.Abstractions;
+
+namespace Demo.ProjectEuler.Tests._0119
+{
+	public class DigitalPowerSumTest : BaseTest
+	{
+		private readonly DigitalPowerSum _sut = new DigitalPowerSum();
+
+		public DigitalPowerSumTest(ITestOutputHelper output) : base(output)
+		{
+		}
+	}
+
+	public class DigitalPowerSum
+	{
+	}
+}
