0c84c5c43c8383c40dd59de4db469622f11c3371
dance2die <dontbothermewithspam@gmail.com>
2016-08-22 09:20:55 -0400

    PE-0119: Created a failing test TestDigitPowerSum

--- Demo.ProjectEuler/Demo.ProjectEuler.Tests/0119/DigitalPowerSumTest.cs
+++ Demo.ProjectEuler/Demo.ProjectEuler.Tests/0119/DigitalPowerSumTest.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Demo.ProjectEuler.Tests.Core;
+﻿using Demo.ProjectEuler.Tests.Core;
+using Xunit;
 using Xunit.Abstractions;
 
 namespace Demo.ProjectEuler.Tests._0119
@@ -15,9 +11,27 @@ namespace Demo.ProjectEuler.Tests._0119
 		public DigitalPowerSumTest(ITestOutputHelper output) : base(output)
 		{
 		}
+
+		[Theory]
+		[InlineData(1, false)]
+		[InlineData(9, false)]
+		[InlineData(512, true)]
+		[InlineData(614656, true)]
+		public void TestDigitPowerSum(int number, bool expected)
+		{
+			bool actual = _sut.IsDigitPower(number);
+
+			Assert.Equal(expected, actual);
+		}
 	}
 
 	public class DigitalPowerSum
 	{
+		public bool IsDigitPower(int number)
+		{
+			if (number < 10) return false;	// by definition
+
+			return false;
+		}
 	}
 }
