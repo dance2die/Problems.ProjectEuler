41fe2bf3469e414fddb407fc8284851518044315
dance2die <dontbothermewithspam@gmail.com>
2016-08-25 10:33:29 -0400

    PE-0124: Added OrderedRadicalsTest

---
+++ Demo.ProjectEuler/Demo.ProjectEuler.Tests/0124/OrderedRadicalsTest.cs
@@ -0,0 +1,18 @@
+﻿using Demo.ProjectEuler.Tests.Core;
+using Xunit.Abstractions;
+
+namespace Demo.ProjectEuler.Tests._0124
+{
+	public class OrderedRadicalsTest : BaseTest
+	{
+		private readonly OrderedRadicals _sut = new OrderedRadicals();
+
+		public OrderedRadicalsTest(ITestOutputHelper output) : base(output)
+		{
+		}
+	}
+
+	public class OrderedRadicals
+	{
+	}
+}
