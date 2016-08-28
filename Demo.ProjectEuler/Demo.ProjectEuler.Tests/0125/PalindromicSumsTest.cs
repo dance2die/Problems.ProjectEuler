cf08b6449292140fbbd555bed8fce067351b4fd7
dance2die <dontbothermewithspam@gmail.com>
2016-08-28 16:43:46 -0400

    PE-0125: Created PalindromicSumsTest

---
+++ Demo.ProjectEuler/Demo.ProjectEuler.Tests/0125/PalindromicSumsTest.cs
@@ -0,0 +1,18 @@
+﻿using Demo.ProjectEuler.Tests.Core;
+using Xunit.Abstractions;
+
+namespace Demo.ProjectEuler.Tests._0125
+{
+	public class PalindromicSumsTest : BaseTest
+	{
+		private readonly PalindromicSums _sut = new PalindromicSums();
+
+		public PalindromicSumsTest(ITestOutputHelper output) : base(output)
+		{
+		}
+	}
+
+	public class PalindromicSums
+	{
+	}
+}
