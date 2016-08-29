c5b5163a69ec1f00d5b2c61e00f39e521e1bd2c2
dance2die <dontbothermewithspam@gmail.com>
2016-08-29 06:56:53 -0400

    PE-0125: Created a failing test

--- Demo.ProjectEuler/Demo.ProjectEuler.Tests/0125/PalindromicSumsTest.cs
+++ Demo.ProjectEuler/Demo.ProjectEuler.Tests/0125/PalindromicSumsTest.cs
@@ -1,4 +1,6 @@
-﻿using Demo.ProjectEuler.Tests.Core;
+﻿using Demo.ProjectEuler.Core;
+using Demo.ProjectEuler.Tests.Core;
+using Xunit;
 using Xunit.Abstractions;
 
 namespace Demo.ProjectEuler.Tests._0125
@@ -10,9 +12,38 @@ namespace Demo.ProjectEuler.Tests._0125
 		public PalindromicSumsTest(ITestOutputHelper output) : base(output)
 		{
 		}
+
+		[Fact]
+		public void TestPalindromicSumForTestData()
+		{
+			const int input = 595;
+			bool actual = _sut.IsPalindromicSum(595);
+
+			const bool expected = true;
+			Assert.Equal(expected, actual);
+		}
 	}
 
 	public class PalindromicSums
 	{
+		private readonly Palindrome _palindrome = new Palindrome();
+
+		public bool IsPalindromicSum(int n)
+		{
+			//int i = 2;
+
+			//do
+			//{
+			//	int offset = 0;
+			//	int currentValue = 0;
+			//	while (currentValue <= n)
+			//	{
+					
+			//	}
+
+			//	i++;
+			//} while (true);
+			return false;
+		}
 	}
 }
