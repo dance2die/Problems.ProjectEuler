235a6ae99d9eee20b2e51d6dc99e42becc73a075
dance2die <dontbothermewithspam@gmail.com>
2016-09-20 12:18:07 -0400

    PE-0127: TestForRadValue passes.

--- Demo.ProjectEuler/Demo.ProjectEuler.Tests/0127/AbcHitsTest.cs
+++ Demo.ProjectEuler/Demo.ProjectEuler.Tests/0127/AbcHitsTest.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Demo.ProjectEuler.Core;
 using Demo.ProjectEuler.Tests.Core;
+using Xunit;
 using Xunit.Abstractions;
 
 namespace Demo.ProjectEuler.Tests._0127
@@ -15,9 +17,28 @@ namespace Demo.ProjectEuler.Tests._0127
 		public AbcHitsTest(ITestOutputHelper output) : base(output)
 		{
 		}
+
+		[Fact]
+		public void TestForRadValue()
+		{
+			const int n = 504;
+			long actual = _sut.GetRad(n);
+
+			const int expected = 42;
+			Assert.Equal(expected, actual);
+		}
 	}
 
 	public class AbcHits
 	{
+		private readonly Prime _prime = new Prime();
+
+		public long GetRad(int n)
+		{
+			var distinctPrimeFactors = _prime.GetPrimeFactors(n).Distinct();
+			var result = (long) distinctPrimeFactors.Aggregate((accu, val) => accu * val);
+
+			return result;
+		}
 	}
 }
