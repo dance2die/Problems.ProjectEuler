8720c736f22c115634c92cfc5d12561896e4d342
dance2die <dontbothermewithspam@gmail.com>
2016-09-20 11:55:41 -0400

    PE-0127: Created AbcHitsTest

---
+++ Demo.ProjectEuler/Demo.ProjectEuler.Tests/0127/AbcHitsTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Demo.ProjectEuler.Tests.Core;
+using Xunit.Abstractions;
+
+namespace Demo.ProjectEuler.Tests._0127
+{
+	public class AbcHitsTest : BaseTest
+	{
+		private readonly AbcHits _sut = new AbcHits();
+
+		public AbcHitsTest(ITestOutputHelper output) : base(output)
+		{
+		}
+	}
+
+	public class AbcHits
+	{
+	}
+}
