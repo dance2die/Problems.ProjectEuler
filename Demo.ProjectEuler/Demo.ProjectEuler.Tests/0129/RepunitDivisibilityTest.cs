f6e0c222946ab07fb4e22cee5e86a1cb4aa7c946
dance2die <dontbothermewithspam@gmail.com>
2016-09-28 09:15:04 -0400

    PE-0129: Created RepunitDivisibilityTest

---
+++ Demo.ProjectEuler/Demo.ProjectEuler.Tests/0129/RepunitDivisibilityTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Demo.ProjectEuler.Tests.Core;
+using Xunit.Abstractions;
+
+namespace Demo.ProjectEuler.Tests._0129
+{
+	public class RepunitDivisibilityTest : BaseTest
+	{
+		private readonly RepunitDivisibility _sut = new RepunitDivisibility();
+
+		public RepunitDivisibilityTest(ITestOutputHelper output) : base(output)
+		{
+		}
+	}
+
+	public class RepunitDivisibility
+	{
+	}
+}
